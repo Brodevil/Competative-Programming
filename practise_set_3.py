534c7231142116e2841133a78ea839dc7102c7b3
Brodevil <brodevil89@gmail.com>
2021-04-18 14:44:20 +0530

    Written the question of set3

---
+++ practise_set_3.py
@@ -0,0 +1,12 @@
+# Practise set 3 - Foods and Calories
+"""
+You visit a resturant called Dance with Devil and food items in this resturant are sorted based on their amount of calories.
+You have to reverse this list of food items and their calories
+
+You have to use following three methods to reverse this list :
+
+    - Inbuilt method of python
+    - Listname[::-1] slicing tricks
+    - Swapping first element with last one and second with second last one so on...
+
+"""
