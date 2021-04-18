91a9f8a1a0e79a678c4b57dab6054590a1d005ab
Brodevil <brodevil89@gmail.com>
2021-04-18 20:15:28 +0530

    Written the problem statement

---
+++ practise_set_4.py
@@ -0,0 +1,24 @@
+# Practise set 4 - The next Polindrome (10 points)
+"""
+The Next Polindrome:
+
+Problem content:
+A palindrome is a string which when revedrse it equal to itself. Example of palindrome includes
+616, mom, 676, 10001.
+You have to take a number as an input from teh user. You have to find the next palindrome 
+corresponding to that number. Your first input should be 'number of the text cases' And then
+take all the cases as input from the user.
+
+Input:
+3
+451
+10
+2133
+
+Output:
+Next polindrome for 451 is 454
+Next polindrome for 10 is 11
+Next polindrome for 2133 is 2222
+
+"""
+
