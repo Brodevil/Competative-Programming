f6f5fb3e947b34bdc323da02250dc1eac5f529b7
Brodevil <brodevil89@gmail.com>
2021-04-19 14:23:31 +0530

    Written the problem statement

---
+++ practise_set_5.py
@@ -0,0 +1,20 @@
+# Python Practise problem 5 (Easy, 10 points) = Palindromify the list
+"""
+Palindromify the list :-
+
+You are given a array which contains some numbers. You have to print a list of next palindromes only if 
+the number is greater then 10 otherwaise you will print that number.
+
+Input:
+[1, 6, 87, 43]
+
+Output:
+[1, 6, 88, 44]
+
+"""
+
+
+# Author : Abhinav
+# Date : 19 April 2021
+# Pourpose : For more python practise 
+
