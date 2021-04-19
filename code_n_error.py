6596e5ce151c1bb8de6d778ffbb1cf7dc9068bae
Brodevil <brodevil89@gmail.com>
2021-04-19 16:42:33 +0530

    Created the .gitignore file

--- code_n_error.py
+++ code_n_error.py
@@ -1,10 +1,10 @@
-import string
+# import string
 
-alphabet_string = string.ascii_lowercase
-# Create a string of all lowercase letters
-print(alphabet_string)
+# alphabet_string = string.ascii_lowercase
+# # Create a string of all lowercase letters
+# print(alphabet_string)
 
 
-import sys
-sys.exit()
-print("I this printing or not")
+# import sys
+# sys.exit()
+# print("I this printing or not")
