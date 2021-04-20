7f6a664ab45f456dc72161b235800ecb1b1ed0a6
Brodevil <brodevil89@gmail.com>
2021-04-20 16:31:10 +0530

    Question completed

--- practise_set_6.py
+++ practise_set_6.py
@@ -7,12 +7,38 @@ Generate a randome integer from a and b. You and your friend have to guess a num
 both are inputs taken from the user. Your friend is player 1 and plays first. He will have to keep choosing the number and 
 your program must tell whether the number is greater than the number. You playteh same game and then the person with minimum number of trials wins!
 
+Randomly generate a number after taking a and b taking input and don't show that number to the user.
+
 
 Input:
 Enter the value of a :
+4
 Enter the value of b :
+13
 
 
 Output:
+Player 1:
+Please guess the number between 4 and 13
+
+5
+Wrong! Guess a greater number again
+
+8 
+Wrong! Guess a greater number again
+
+
+6 
+Correct You tooked 3 trials to guess the number
+
+Player 2:
+Please guess the number between 4 and 13
+.
+.
+.
+Correct You tooked 7 trials to guess the number
+
+>>> Player 1 wins as He tooked 3 trials and 
 
 """
+
