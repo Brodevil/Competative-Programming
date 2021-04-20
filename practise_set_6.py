9e0fdefa15a8b68cb4943d962ee0c2fca5ff8852
Brodevil <brodevil89@gmail.com>
2021-04-20 15:54:53 +0530

    written question of set 6

---
+++ practise_set_6.py
@@ -0,0 +1,18 @@
+# Python practise problem 6 (Easy, 10 points) - Guess the number
+
+"""
+Guess the number :
+
+Generate a randome integer from a and b. You and your friend have to guess a number between tow number a and b and 
+both are inputs taken from the user. Your friend is player 1 and plays first. He will have to keep choosing the number and 
+your program must tell whether the number is greater than the number. You playteh same game and then the person with minimum number of trials wins!
+
+
+Input:
+Enter the value of a :
+Enter the value of b :
+
+
+Output:
+
+"""
