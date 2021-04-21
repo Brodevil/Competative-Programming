ee9480fa72b0c1fbe42486d5000ca659baeb04bc
Brodevil <brodevil89@gmail.com>
2021-04-21 12:15:55 +0530

    .

--- practise_set_7.py
+++ practise_set_7.py
@@ -1,27 +1,27 @@
-# Python Practise Set 7 (Medium, 10 points) -  Search Engine
-"""
-Search Engine:
-
-You are given few sentences as a list (Python list of sentences).Take a query string as a input from the user.
-You have to pull out the sentences matching a query inputted by the user in decreasing order of relevance after convertin every word in the query and the sentence to lowercase.
-Most relevent sentence is the one with the maximum number of matching words with query.
-
-Secntence = ["This is practise ", "this string will be too big that we will not able to even find anything sir", "Python is too best programming laguages", "Python Porgramming with PyCharm and VS code", "Best python pratise is to just practise and learn many things"]
-
-
-Input:
-
-Please input your query string :
-"python"
-
-
-Output:
-
-About 3 result (00.89 sec.):
-
-1. Best python pratise is to just practise and learn many things
-2. Python Porgramming with PyCharm and VS code
-3. Python is too best programming laguages
+# Python Practise Set 7 (Medium, 10 points) -  Search Engine 
+""" 
+Search Engine: 
  
+You are given few sentences as a list (Python list of sentences).Take a query string as a input from the user. 
+You have to pull out the sentences matching a query inputted by the user in decreasing order of relevance after convertin every word in the query and the sentence to lowercase. 
+Most relevent sentence is the one with the maximum number of matching words with query. 
+ 
+Secntence = ["This is practise ", "this string will be too big that we will not able to even find anything sir", "Python is too best programming laguages", "Python Porgramming with PyCharm and VS code", "Best python pratise is to just practise and learn many things"] 
+ 
+ 
+Input:
+ 
+Please input your query string : 
+"python" 
+ 
+ 
+Output: 
+ 
+About 3 result (00.89 sec.): 
+ 
+1. Best python pratise is to just practise and learn many things 
+2. Python Porgramming with PyCharm and VS  
+3. Python is too best programming laguages 
+
 
 """
