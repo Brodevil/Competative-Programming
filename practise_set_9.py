e1fc7de29e4fca708ce5fd21e41b78d07b254327
Brodevil <brodevil89@gmail.com>
2021-04-22 12:28:17 +0530

    Written the program problem statement

---
+++ practise_set_9.py
@@ -0,0 +1,31 @@
+# Python practise problem 9 (Medium, 20 points) = Jumpbled Funny Names
+
+"""
+Jumpbled funny names:
+
+Its result day at school and not everyone is happy. You decided to make y our friedn laught by jumbling their 
+names to come up with some funny names.
+
+You program should take the number of the names and the names separated by spaces as input. Output
+should be funny names i9n the same order.
+
+
+Input:
+Enter number of frineds:
+3
+
+Enter the names of your 3 friends :
+Abhinav Choudhary
+Sibam Gopale
+Aniket bhagat
+
+
+Output:
+
+Abhinav Gopale
+Aniekt choudhary
+Sibam Bhagat 
+# I will it meanse Abhinav will make more effective to this program and make laugh as I will make the computer to speak also in english which will defenatly make laught to the user lol
+
+
+"""
