16381d76c9f67c2b3b437d570301ddde83e024ce
Brodevil <brodevil89@gmail.com>
2021-04-22 20:33:02 +0530

    just nothing

--- practise_set_10.py
+++ practise_set_10.py
@@ -1,4 +1,4 @@
-# Python Practise 10- From the Hacker Rank(Write a function)
+# Python Practise 10- From the Hacker Rank (Write a function)
 
 """
 An extra day is added to the calendar almost every four years as February 29, and the day is called a leap day. It corrects the calendar for the fact that our planet takes approximately 365.25 days to orbit the sun. A leap year contains a leap day.
@@ -58,5 +58,9 @@ def is_leap(year):
     else:
         return False
 
-year = int(input())
-print(is_leap(year))
+
+if __name__ == "__main__":
+    year = int(input())
+    print(is_leap(year))
+
+
