601607f7a9c267c0ba49d258d688f039520bec3f
Brodevil <brodevil89@gmail.com>
2021-04-23 10:28:17 +0530

    Written the problem statement and also code

--- practise_set_12.py
+++ practise_set_12.py
@@ -1,1 +1,23 @@
-# Python practise 12 (Easy) - 
+# Python practise 12 (Easy) - Indian Kirana Shop
+
+"""
+Indian Kirana shop keeper calculator :
+
+Created the python program that will keep adding a steam of numbers inputted by the user.
+The adding stops as soon as user presses q key on the keyboard.
+"""
+
+if __name__ == "__main__":
+    print("Welcome to the Indian Kirana shop keeper's Calculator python program \nKeep Entering your value you wants to add, And to stop the program and get result press 'q'")
+    sum = 0
+    while True:
+        value = input("Enter the value and press Enter :\t")
+        if value.lower() == "q":
+            break
+        else:
+            try:
+                sum += int(value)
+            except ValueError:
+                print("Enter a league input that will a number or press q to quite.\n")
+        
+    print(f"Your total sum is: {sum:>10}\nThank you for using our Caculator")
