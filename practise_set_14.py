c8dc235596582f654de64c0a6c8c6166fc150a4b
Brodevil <brodevil89@gmail.com>
2021-04-23 14:29:03 +0530

    Written the problem statement and I will start to code

---
+++ practise_set_14.py
@@ -0,0 +1,15 @@
+# Python Practise 14 - Currency Converter
+"""
+Created a Currency converter python program that will convert your Currency to several Currency
+The currency data is available in the currency.txt file in this directory.
+
+Using those data You have to create the python program to convert any Currency to any other Currency
+"""
+
+# Author = Abhinav
+# Date = 23 April 2021
+# Purpose = Just for the python practise to become expert in the python programming and logic building
+
+
+if __name__ == '__main__':
+    pass
