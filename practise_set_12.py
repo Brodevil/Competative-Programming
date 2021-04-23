3f0134d5fed5de50ee9078f885b3d4751ab22bef
Brodevil <brodevil89@gmail.com>
2021-04-23 10:37:34 +0530

    Solved one problem inside this i.e. printing the bill

--- practise_set_12.py
+++ practise_set_12.py
@@ -5,20 +5,33 @@ Indian Kirana shop keeper calculator :
 
 Created the python program that will keep adding a steam of numbers inputted by the user.
 The adding stops as soon as user presses q key on the keyboard.
+
+Additionlly at last Show the customer how much prices I bought
+
+Example:
+Shree Ram Kirana:
+1. #20
+2. #30
+3. #60
+And then totale
 """
 
 if __name__ == "__main__":
     print("Welcome to the Indian Kirana shop keeper's Calculator python program \nKeep Entering your value you wants to add, And to stop the program and get result press 'q'")
-    sum = 0
+    totalSum = list()
     while True:
         value = input("Enter the value and press Enter :\t")
         if value.lower() == "q":
             break
         else:
             try:
-                sum += int(value)
-                print(f"Order Total so far : {sum}\n")
+                totalSum.append(int(value))
+                print(f"Order Total so far : {sum(totalSum)}\n")
             except ValueError:
                 print("Enter a league input that will a number or press q to quite.\n")
         
-    print(f"Your Bill total is: {sum:>10}\nThank you for Shoping with us")
+    print(f"Your Bill total is: {sum(totalSum):>10}\nThank you for Shoping with us\n\n")
+
+    print("Shree Ram's Shop Bill :")
+    for index, value in enumerate(totalSum):
+        print(f"{index+1}. #{value} Rupees item")
