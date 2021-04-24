aca497bf0ee76ef2ff3297143c1def01cb0afcb9
Brodevil <brodevil89@gmail.com>
2021-04-24 20:49:09 +0530

    created one more reminding function

--- practise_set_18.py
+++ practise_set_18.py
@@ -15,16 +15,17 @@ import time
 from plyer import notification
 if __name__ == '__main__':
     while True:
+        time.sleep(6)
         notification.notify(
             title = "Attept 20-20-20 Eyes Exercise",
             message="After every 20 minutes to give rest to your eyes see 20 meter long from your original distance in nature for 20 sec. Which defenatly safe the your eyes",
-            app_icon = "",
+            app_icon = r"E:\ADMIN\Pictures\Saved Pictures\abhinav.jpg",
             timeout= 12
         )
+        time.sleep(20)
         notification.notify(
             title = "Plz Grap A Glass Of Water!",
             message ="The National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids for men. About 11.5 cups (2.7 liters) of fluids a day for women.",
             app_icon = r"E:\ADMIN\Pictures\Saved Pictures\drinkwater.ico",
             timeout= 10
             )
-        time.sleep(60*20)
