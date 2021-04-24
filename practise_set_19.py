2a6cdb2a3279f19968163b8efdba2666a38b0198
Brodevil <brodevil89@gmail.com>
2021-04-24 21:37:33 +0530

    Created just a boilerplate

---
+++ practise_set_19.py
@@ -0,0 +1,13 @@
+# Python practise 19 (From Hackerrank) = 
+
+"""
+
+
+"""
+
+# Author = Abhinav
+# Date = 24 April 2021
+# Motive = Just for the python practise to not just learn python also to expert it
+
+if __name__ == '__main__':
+	pass
