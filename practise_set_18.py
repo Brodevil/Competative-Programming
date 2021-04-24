86c7c6f581d6a07b5d05a5070bca039c8276b45a
Brodevil <brodevil89@gmail.com>
2021-04-24 22:21:32 +0530

    Writtend the question

--- practise_set_18.py
+++ practise_set_18.py
@@ -23,6 +23,7 @@ if __name__ == '__main__':
             timeout= 12
         )
         time.sleep(20)
+        
         notification.notify(
             title = "Plz Grap A Glass Of Water!",
             message ="The National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids for men. About 11.5 cups (2.7 liters) of fluids a day for women.",
