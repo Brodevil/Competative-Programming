f91ca29eb3c47a7ba8072443503914c5d8ea352b
Brodevil <brodevil89@gmail.com>
2021-04-26 22:42:56 +0530

    Solved the problem

--- practiset_set_25.py
+++ practiset_set_25.py
@@ -60,7 +60,8 @@ A string is a collection of characters.
 #
 
 def print_full_name(first, last):
-    # Write your code here
+    print(f"Hello {first} {last}! You just delved into python.")
+
 
 if __name__ == '__main__':
     first_name = input()
