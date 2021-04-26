e240663afbb86cca0a36a3127f0bcba79999c671
Brodevil <brodevil89@gmail.com>
2021-04-26 12:52:09 +0530

    Written the problem statement

--- practise_set_22.py
+++ practise_set_22.py
@@ -3,6 +3,33 @@
 """
 Tuple :
 
+Task
+Given an integer, n , and n space-separated integers as input, create a tuple, t, of those
+n integers. Then compute and print the result of hash(t).
+
+Note: hash() is one of the functions in the __builtins__ module, 
+so it need not be imported.
+
+
+Input Format :
+
+The first line contains an integer, n, denoting the number of elements in the tuple.
+The second line contains n space-separated integers describing the elements in tuple t.
+
+
+Output Format :
+
+Print the result of hash(t).
+
+
+
+Sample Input 0 :
+2
+1 2
+
+
+Sample Output 0 :
+3713081631934410656
 
 
 """
@@ -10,3 +37,4 @@ Tuple :
 # Author = Abhinav
 # Date = 26 April 2021
 # Motive = Just for the python practise to not just learn python also to expert it
+
