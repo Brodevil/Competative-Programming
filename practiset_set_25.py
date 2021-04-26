a2f50be9baa8a8698e7722356fbda7006f4ded6b
Brodevil <brodevil89@gmail.com>
2021-04-26 22:40:17 +0530

    Wirtten the problem statement

---
+++ practiset_set_25.py
@@ -0,0 +1,68 @@
+# Python practise 25 (From hackerrank) = What's your name
+
+"""
+What's your name :
+
+You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+
+>>> Hello firstname lastname! You just delved into python.
+
+
+Function Description :
+Complete the print_full_name function in the editor below.
+
+print_full_name has the following parameters:
+
+ - string first: the first name
+ - string last: the last name
+
+
+Prints :
+string: 'Hello firstname lastname! You just delved into python' where firstname and lastname are replaced with first and last.
+
+
+Input Format :
+The first line contains the first name, and the second line contains the last name.
+
+
+Constraints :
+The length of the first and last names are each ≤ 10.
+
+
+Sample Input 0 :
+Ross
+Taylor
+
+
+Sample Output 0 :
+
+Hello Ross Taylor! You just delved into python.
+
+Explanation 0 :
+The input read by the program is stored as a string data type. 
+A string is a collection of characters.
+
+
+"""
+
+# Author = Abhinav
+# Date = 26 April 2021
+# Motive = Just for the python practise to not just learn python also to expert it
+
+
+
+# Complete the 'print_full_name' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+#
+
+def print_full_name(first, last):
+    # Write your code here
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
