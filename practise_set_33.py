f0d17b7ac6b898026411cb9e03213195d0b813de
Brodevil <brodevil89@gmail.com>
2021-05-10 22:05:03 +0530

    Tring to solve the quesiotn

--- practise_set_33.py
+++ practise_set_33.py
@@ -21,7 +21,7 @@ This method returns a centered string of length width.
 
 
 >>> width = 20
->>> print 'HackerRank'.center(width,'-')
+>>> print('HackerRank'.center(width,'-'))
 -----HackerRank-----
 
 
@@ -29,7 +29,7 @@ This method returns a centered string of length width.
 This method returns a right aligned string of length width.
 
 >>> width = 20
->>> print 'HackerRank'.rjust(width,'-')
+>>> print('HackerRank'.rjust(width,'-'))
 ----------HackerRank
 
 
@@ -100,21 +100,21 @@ c = 'H'
 
 # Top Cone
 for i in range(thickness):
-    print((c * i).______(thickness - 1) + c + (c * i).______(thickness - 1))
+    print((c * i).rjust(thickness - 1) + c + (c * i).ljust(thickness - 1))
 
 # Top Pillars
 for i in range(thickness + 1):
-    print((c * thickness).______(thickness * 2) + (c * thickness).______(thickness * 6))
+    print((c * thickness).ljust(thickness * 2) + (c * thickness).rjust(thickness * 6))
 
-# Middle Belt
-for i in range((thickness + 1) // 2):
-    print((c * thickness * 5).______(thickness * 6))
-
-# Bottom Pillars
-for i in range(thickness + 1):
-    print((c * thickness).______(thickness * 2) + (c * thickness).______(thickness * 6))
-
-# Bottom Cone
-for i in range(thickness):
-    print(((c * (thickness - i - 1)).______(thickness) + c + (c * (thickness - i - 1)).______(thickness)).______(
-        thickness * 6))
+# # Middle Belt
+# for i in range((thickness + 1) // 2):
+#     print((c * thickness * 5).______(thickness * 6))
+#
+# # Bottom Pillars
+# for i in range(thickness + 1):
+#     print((c * thickness).______(thickness * 2) + (c * thickness).______(thickness * 6))
+#
+# # Bottom Cone
+# for i in range(thickness):
+#     print(((c * (thickness - i - 1)).______(thickness) + c + (c * (thickness - i - 1)).______(thickness)).______(
+#         thickness * 6))
