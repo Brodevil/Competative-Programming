235d8ed3e6f4e825a8feffc5dc13fdd2da2df0e6
Brodevil <brodevil89@gmail.com>
2021-05-18 12:30:33 +0530

    Initial commit

--- practise_set_33.py
+++ practise_set_33.py
@@ -104,8 +104,7 @@ for i in range(thickness):
 
 # Top Pillars
 for i in range(thickness + 1):
-    print((c * thickness).ljust(thickness * 2) + (c * thickness).rjust(thickness * 6))
-
+    print((c * thickness).rjust(thickness * 2) + (c * thickness).rjust(thickness * 6))
 # # Middle Belt
 # for i in range((thickness + 1)    // 2):
 #     print((c * thickness * 5).______(thickness * 6))
