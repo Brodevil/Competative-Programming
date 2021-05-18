a23f4082eab183b2693ae0e44a625bcb510094cb
Brodevil <brodevil89@gmail.com>
2021-05-18 13:52:00 +0530

    Solved the problem

--- practise_set_33.py
+++ practise_set_33.py
@@ -104,16 +104,18 @@ for i in range(thickness):
 
 # Top Pillars
 for i in range(thickness + 1):
-    print((c * thickness).rjust(thickness * 2) + (c * thickness).rjust(thickness * 6))
-# # Middle Belt
-# for i in range((thickness + 1)    // 2):
-#     print((c * thickness * 5).______(thickness * 6))
-#
-# # Bottom Pillars
-# for i in range(thickness + 1):
-#     print((c * thickness).______(thickness * 2) + (c * thickness).______(thickness * 6))
-#
-# # Bottom Cone
-# for i in range(thickness):
-#     print(((c * (thickness - i - 1)).______(thickness) + c + (c * (thickness - i - 1)).______(thickness)).______(
-#         thickness * 6))
+    print((c * thickness).center(thickness*2) + (c * thickness).center(thickness * 6))
+
+
+# Middle Belt
+for i in range((thickness + 1) // 2):
+    print((c * thickness * 5).center(thickness * 6))
+
+# Bottom Pillars
+for i in range(thickness + 1):
+    print((c * thickness).center(thickness * 2) + (c * thickness).center(thickness * 6))
+
+# Bottom Cone
+for i in range(thickness):
+    print(((c * (thickness - i - 1)).rjust(thickness) + c + (c * (thickness - i - 1)).ljust(thickness)).rjust(
+        thickness * 6))
