4f04b15bab92e3ada5e83801ee0dedc73346dd3a
Brodevil <brodevil89@gmail.com>
2021-05-23 19:15:14 +0530

    Solved the question

--- practise_set_34.py
+++ practise_set_34.py
@@ -56,5 +56,13 @@ YZ
 
 import textwrap
 
+
+def wrap(string, max_width):
+    return "\n".join(textwrap.wrap(string, max_width))
+
+
 if __name__ == '__main__':
-    pass
+    string, max_width = input(), int(input())
+    print(wrap(string, max_width))
+
+
