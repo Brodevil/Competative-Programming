a5549b486423d1777de47f04394cc07b95e2f7ed
Brodevil <brodevil89@gmail.com>
2021-06-07 23:15:22 +0530

    Remind to solve and wite half question

---
+++ practise_set_40.py
@@ -0,0 +1,30 @@
+# Python practise 40 = (From hackerrank) = Merge the Tools!   (https://www.hackerrank.com/challenges/merge-the-tools)
+
+"""
+Merge the Tools!
+
+
+Consider the following :
+ - A string, s, of length n where s=c[0] c[1]...[n-1].
+ - An integer, k, where k is a factor of n.
+
+
+We can split s into n/k subsegments where each subsegment, , consists of a contiguous block of
+characters in . Then, use each to create string such that:
+The characters in are a subsequence of the characters in .
+Any repeat occurrence of a character is removed from the string such that each character in
+occurs exactly once. In other words, if the character at some index in occurs at a previous index
+in , then do not include the character in string .
+Given and , print lines where each line denotes string .
+"""
+
+
+
+# Author = Abhinav
+# Date = June 7 2021
+# Pourpose = Just for python practicing will lots of dedications
+
+
+
+if __name__ == '__main__':
+    pass
