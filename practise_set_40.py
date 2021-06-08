6ec76cd731ab6edaf0b54b6f4699286ba92561e6
Brodevil <brodevil89@gmail.com>
2021-06-08 23:04:42 +0530

    written the question

--- practise_set_40.py
+++ practise_set_40.py
@@ -9,13 +9,80 @@ Consider the following :
  - An integer, k, where k is a factor of n.
 
 
-We can split s into n/k subsegments where each subsegment, , consists of a contiguous block of
-characters in . Then, use each to create string such that:
-The characters in are a subsequence of the characters in .
-Any repeat occurrence of a character is removed from the string such that each character in
-occurs exactly once. In other words, if the character at some index in occurs at a previous index
-in , then do not include the character in string .
-Given and , print lines where each line denotes string .
+We can split S into n/k substrings where each subtring, t, consists of a contiguous block of  characters in k.
+Then, use each t to create string u[i] such that:
+
+ - The characters in  are a subsequence of the characters in t.
+ - Any repeat occurrence of a character is removed from the string such that each character in u[i] occurs exactly once. 
+   In other words, if the character at some index j in t[i] occurs at a previous index <j in t[i], then do not include the character in string u[i].
+
+
+Given s and k, print n/k lines where each line i denotes string u[i]. 
+
+
+Example :-
+s= 'AAABCADDE' 
+k=3
+
+
+There are three substrings of length  to consider: 'AAA', 'BCA' and 'DDE'. The first substring is all 'A' characters, so u[1] = 'A'. 
+The second substring has all distinct characters, so u[2] = 'BCA'. 
+The third substring has 2 different characters, so u[3]= 'DE'. Note that a subsequence maintains the original order of characters encountered. 
+The order of characters in each subsequence shown is important.
+
+
+Function Description :
+
+Complete the merge_the_tools function in the editor below.
+
+merge_the_tools has the following parameters:
+
+ - string s: the string to analyze
+ - int k: the size of substrings to analyze
+
+
+Prints :
+Print each subsequence on a new line. There will be n/k of them. No return value is expected.
+
+
+
+Input Format :
+
+The first line contains a single string, s.
+The second line contains an integer, k, the length of each substring.
+
+
+Constraints :
+ - 1<= n <= 10**4, where  is the length of 
+ - 1 <= k <= n
+
+It is guaranteed that n is a multiple of k.
+
+
+Sample Input :
+
+STDIN       Function
+-----       --------
+AABCAAADA   s = 'AABCAAADA'
+3           k = 3
+
+
+
+Sample Output :
+AB
+CA
+AD
+
+
+Explanation :
+
+Split  into  equal parts of length . Convert each  to  by removing any subsequent occurrences of non-distinct characters in :
+
+
+(here many thigs are ther just read it out by the given link) (https://www.hackerrank.com/challenges/merge-the-tools/problem)
+
+Print each  on a new line.
+
 """
 
 
@@ -25,6 +92,9 @@ Given and , print lines where each line denotes string .
 # Pourpose = Just for python practicing will lots of dedications
 
 
+def merge_the_tools(string, k):
+    # your code goes here
 
 if __name__ == '__main__':
-    pass
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
