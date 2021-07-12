6790cc3c12927bed79511cdbb669aa17935fa0aa
Brodevil <brodevil89@gmail.com>
2021-07-12 19:10:50 +0530

    created the file, remain to wite question and solve it

---
+++ Practise Sets/practise_set_45.py
@@ -0,0 +1,43 @@
+# Python practise 45 = Staircase (From hackerrank) = (https://www.hackerrank.com/challenges/staircase/problem)
+
+
+"""
+Staircase :
+
+
+
+
+
+"""
+
+# Author = Abhinav
+# Date = 12 July 2021
+# Pourpose = Now I am getting very less time to touch my laptop, so in few time lets Practise some new thing in Python
+# Source = https://www.hackerrank.com/challenges/staircase/problem
+
+
+# Solution :
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def staircase(n):
+    # Write your code here
+    pass
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
