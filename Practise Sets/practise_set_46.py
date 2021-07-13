7b4b303f61dbfb16a5bf43c633d4daa9d3176101
Brodevil <brodevil89@gmail.com>
2021-07-13 19:19:37 +0530

    Initial commit

---
+++ Practise Sets/practise_set_46.py
@@ -0,0 +1,46 @@
+# Python Practise 46 - Mini-Max Sum (From hackerrank) = (https://www.hackerrank.com/challenges/mini-max-sum/problem)
+
+
+"""
+Mini-Max Sum | Problem statement
+
+
+
+
+
+"""
+
+
+# Author = Abhinav
+# Date = 13 July 2021
+# Pourpose = Now I am getting very less time to touch my laptop, so in few time lets Practise some new thing in Python
+# Source = https://www.hackerrank.com/challenges/mini-max-sum/problem
+
+
+# Solution :
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    # Write your code here
+    pass
+
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
