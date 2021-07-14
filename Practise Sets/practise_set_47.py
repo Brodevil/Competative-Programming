82f69270156686a312203ca4ed0f47e373b24ccd
Brodevil <brodevil89@gmail.com>
2021-07-14 19:01:09 +0530

    Answer template but not solved

--- Practise Sets/practise_set_47.py
+++ Practise Sets/practise_set_47.py
@@ -56,3 +56,45 @@ Candle heights are [3, 2, 1, 3]. The tallest candles are 3 units, and there are 
 
 
 """
+
+
+# Author = Abhinav
+# Date = 14 July 2021
+# Pourpose = Now I am getting very less time to touch my laptop, so in few time lets Practise some new thing in Python
+# Source = https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+
+
+# Soluction :
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    pass
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
