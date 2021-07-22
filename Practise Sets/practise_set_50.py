cf4008f5c2f112c2d542f4b7d703da36e495cfdf
Brodevil <brodevil89@gmail.com>
2021-07-22 20:24:23 +0530

    Next question for tommoroww

---
+++ Practise Sets/practise_set_50.py
@@ -0,0 +1,67 @@
+# Python Practise 50 = Apple and Orange (From hackerrank)
+
+"""
+Apple and Orange | Problem set :
+
+
+Get the Problem set here : https://www.hackerrank.com/challenges/apple-and-orange/problem
+
+"""
+
+# Author = Abhinav
+# Date = 22 July 2021
+# Pourpose = Now I am getting very less time to touch my laptop, so in few time lets Practise some new thing in Python
+# Source = https://www.hackerrank.com/challenges/apple-and-orange/problem
+
+# Solution :
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countApplesAndOranges' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER s
+#  2. INTEGER t
+#  3. INTEGER a
+#  4. INTEGER b
+#  5. INTEGER_ARRAY apples
+#  6. INTEGER_ARRAY oranges
+#
+
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    # Write your code here
+    return 0
+    
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    s = int(first_multiple_input[0])
+
+    t = int(first_multiple_input[1])
+
+    second_multiple_input = input().rstrip().split()
+
+    a = int(second_multiple_input[0])
+
+    b = int(second_multiple_input[1])
+
+    third_multiple_input = input().rstrip().split()
+
+    m = int(third_multiple_input[0])
+
+    n = int(third_multiple_input[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
