f1a260e4c41592fe50a426966bd97156c21c1cc7
Brodevil <brodevil89@gmail.com>
2021-07-25 19:00:56 +0530

    Solved the question yar

--- Practise Sets/practise_set_51.py
+++ Practise Sets/practise_set_51.py
@@ -27,7 +27,30 @@ Problem statement : https://www.hackerrank.com/challenges/3d-surface-area/proble
 
 def surfaceArea(A):
     # Write your code here
-    return 0
+    area = 0
+    neighs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+
+    for x in range(H):
+        for y in range(W):
+            el = A[x][y]
+            # add base and top area
+            area += 2
+            for dx, dy in neighs:
+                try:
+                    # Raise index exception if trying to access index -1
+                    if -1 in [x+dx, y+dy]:
+                        raise IndexError
+
+                    # Add area of actual element from neighbours as base
+                    new = el - A[x+dx][y+dy]
+                    if new > 0:
+                        area += new
+                # Add area corresponding to the sides of the figure
+                except IndexError:
+                    area += el
+        
+    return area
+    
 
 
 if __name__ == '__main__':
