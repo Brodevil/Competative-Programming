d1e95524940e5e6fbcbad3c6f41ac67775c4e123
Brodevil <brodevil89@gmail.com>
2021-07-27 19:00:03 +0530

    new question for tomorrow practise

---
+++ Practise Sets/practise_set_53.py
@@ -0,0 +1,53 @@
+# Python practise 53 = Number Line Jumps (From hackerrank)
+
+
+"""
+Number Line Jumps | Problem statement :
+
+
+Get The problem statement here : https://www.hackerrank.com/challenges/kangaroo/problem
+And solution here |
+
+
+"""
+
+
+# Author = Abhinav
+# Date = 27 July 2021
+# Pourpose = Now I am getting very less time to touch my laptop, so in few time lets Practise some new thing in Python
+# Source = https://www.hackerrank.com/challenges/kangaroo/problem
+
+# Solution :
+
+
+#!/bin/python3
+
+# Complete the 'kangaroo' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x1
+#  2. INTEGER v1
+#  3. INTEGER x2
+#  4. INTEGER v2
+
+
+def kangaroo(x1, v1, x2, v2):
+    # Write your code here
+    return 0
+
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    print(result)
