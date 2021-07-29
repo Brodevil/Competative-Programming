f59eb3a6d8e6ac43115f625def34739f0abea2d1
Brodevil <brodevil89@gmail.com>
2021-07-29 19:45:07 +0530

    Initial commit:

---
+++ Practise Sets/practise_set_55.py
@@ -0,0 +1,17 @@
+# Python Practise 54 = Breaking the Records (From hackerrank)
+
+"""
+Breaking the Records | Problem Statement :
+
+
+Get the Problem statment here : https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+And the soluction here :|
+
+"""
+
+# Author = Abhinav
+# Date = 29 July 2021
+# Pourpose = Now I am getting very less time to touch my laptop, so in few time lets Practise some new thing in Python
+# Source =  https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+
+# Solution :
