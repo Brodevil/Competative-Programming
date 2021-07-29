58a673c91d735d055097f782ce847b7287a5995f
Brodevil <brodevil89@gmail.com>
2021-07-29 19:46:57 +0530

    Basic inbuild Answer template by Hackerrank but not yet solved

--- Practise Sets/practise_set_55.py
+++ Practise Sets/practise_set_55.py
@@ -15,3 +15,25 @@ And the soluction here :|
 # Source =  https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
 
 # Solution :
+
+#!/bin/python3
+
+# Complete the 'breakingRecords' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY scores as parameter.
+
+
+def breakingRecords(scores):
+    # Write your code here
+    return 0
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    print(result)
