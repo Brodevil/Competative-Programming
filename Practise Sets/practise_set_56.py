0fdef7a7710974d50feccb54ee496dee36d95ca1
Brodevil <brodevil89@gmail.com>
2021-07-30 19:11:16 +0530

    Initial commit

---
+++ Practise Sets/practise_set_56.py
@@ -0,0 +1,17 @@
+# Python Practise 54 = Subarray Division (From hackerrank)
+
+"""
+Subarray Division | Problem Statment :
+
+
+Ger the Problem statemnet here : https://www.hackerrank.com/challenges/the-birthday-bar/problem
+And the solution here : |
+
+"""
+
+# Author = Abhinav
+# Date = 30 July 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  https://www.hackerrank.com/challenges/the-birthday-bar/problem
+
+# Solution :
