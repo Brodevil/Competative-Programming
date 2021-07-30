fcbf5222630e6a96d3ead0dd7df89421876e29bd
Brodevil <brodevil89@gmail.com>
2021-07-30 19:12:58 +0530

    Basic inbuild answer template by the hackerrank

--- Practise Sets/practise_set_56.py
+++ Practise Sets/practise_set_56.py
@@ -15,3 +15,34 @@ And the solution here : |
 # Source =  https://www.hackerrank.com/challenges/the-birthday-bar/problem
 
 # Solution :
+
+#!/bin/python3
+
+# Complete the 'birthday' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY s
+#  2. INTEGER d
+#  3. INTEGER m
+
+
+def birthday(s, d, m):
+    # Write your code here
+    return 0
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    first_multiple_input = input().rstrip().split()
+
+    d = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = birthday(s, d, m)
+
+    print(result)
