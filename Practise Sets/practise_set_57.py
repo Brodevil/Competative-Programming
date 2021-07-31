44c901db8c2ea91f574669e3ad680ffff5a18c21
Brodevil <brodevil89@gmail.com>
2021-07-31 19:23:52 +0530

    Basic template of solution inbuild in hackkerank

--- Practise Sets/practise_set_57.py
+++ Practise Sets/practise_set_57.py
@@ -16,3 +16,32 @@ And Get the solved solution here :|
 # Source =  https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
 
 # Solution :
+
+#!/bin/python3
+
+# Complete the 'divisibleSumPairs' function below.
+
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY ar
+
+
+def divisibleSumPairs(n, k, ar):
+    # Write your code here
+    return 0
+
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    print(result)
