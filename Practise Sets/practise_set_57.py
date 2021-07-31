7ec3300ece6b54dbfe590121ed0b4521765cb916
Brodevil <brodevil89@gmail.com>
2021-07-31 19:22:24 +0530

    Initial commit

--- Practise Sets/practise_set_57.py
+++ Practise Sets/practise_set_57.py
@@ -1,7 +1,18 @@
 # Python Practise 57 = Divisible Sum Pairs (From hackerrank)
 
 """
+Divisible Sum Pairs | Problem Statement :
 
+
+Get the Problem Statment on Hackerrank here : https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
+And Get the solved solution here :|
 
 
 """
+
+# Author = Abhinav
+# Date = 31 July 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
+
+# Solution :
