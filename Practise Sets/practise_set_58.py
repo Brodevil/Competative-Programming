25f02e38a4f40151b7378f5413dcfb7d99fc5a43
Brodevil <brodevil89@gmail.com>
2021-08-01 19:13:44 +0530

    Initial commit

---
+++ Practise Sets/practise_set_58.py
@@ -0,0 +1,13 @@
+# Python Practise 58 = Migratory Birds (From hackerrank)
+
+"""
+Migratory Birds | Problem Statement :
+
+
+Get the Problem Statment online on hackerrank : https://www.hackerrank.com/challenges/migratory-birds/problem
+
+And get the Solved solution here :|
+
+
+"""
+
