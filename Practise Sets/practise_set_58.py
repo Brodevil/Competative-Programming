e74e4f1faf60ba5f22b68fa6f26084b2a251c46d
Brodevil <brodevil89@gmail.com>
2021-08-01 19:15:33 +0530

    Basic inbuild hackerrank solution template

--- Practise Sets/practise_set_58.py
+++ Practise Sets/practise_set_58.py
@@ -11,3 +11,33 @@ And get the Solved solution here :|
 
 """
 
+
+# Author = Abhinav
+# Date = 1 April 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  https://www.hackerrank.com/challenges/migratory-birds/problem
+
+# Solution :
+
+
+#!/bin/python3
+
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+
+
+def migratoryBirds(arr):
+    # Write your code here
+    return 0
+
+
+if __name__ == '__main__':
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    print(result)
