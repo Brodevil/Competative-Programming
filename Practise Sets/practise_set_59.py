9007ab99d95428a7376df5da87b5d3681dcbec7b
Brodevil <brodevil89@gmail.com>
2021-08-02 19:06:00 +0530

    Initial commit

---
+++ Practise Sets/practise_set_59.py
@@ -0,0 +1,19 @@
+# Python Practise 58 = Day of the Programmer  (From hackerrank)
+
+"""
+Day of the Programmer | Problem Statment 
+
+
+Get the Problem Statment online on hackerrank : https://www.hackerrank.com/challenges/day-of-the-programmer/problem
+And Get the solved solution in python here :
+
+
+"""
+
+
+# Author = Abhinav
+# Date = 2 April 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  https://www.hackerrank.com/challenges/day-of-the-programmer/problem
+
+# Solution :
