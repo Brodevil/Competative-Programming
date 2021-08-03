e99890bc3645c2c3ebbef01813da43fa7102a02e
Brodevil <brodevil89@gmail.com>
2021-08-03 18:57:40 +0530

    solved the question

--- Practise Sets/practise_set_59.py
+++ Practise Sets/practise_set_59.py
@@ -26,8 +26,13 @@ And Get the solved solution in python here :
 # The function accepts INTEGER year as parameter.
 
 
-def dayOfProgrammer(year):
-    return 0
+def dayOfProgrammer(year: int):
+    if year == 1918:
+        return '26.09.1918'
+    elif ((year <= 1917) & (year%4 == 0)) or ((year > 1918) & (year%400 == 0 or ((year%4 == 0) & (year%100 != 0)))):
+        return '12.09.%s' % year
+    else:
+        return '13.09.%s' % year
 
 
 if __name__ == '__main__':
