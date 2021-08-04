03271b0e976c9c87dc29be305e036a3c526aa3ff
Brodevil <brodevil89@gmail.com>
2021-08-04 19:26:52 +0530

    Basic inbuild solution template by hackerrank

--- Practise Sets/practise_set_61.py
+++ Practise Sets/practise_set_61.py
@@ -19,3 +19,28 @@ And get the solved solution in python here : |
 
 
 # Solution :
+
+
+#!/bin/python3
+
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+
+
+def sockMerchant(n, ar):
+    # Write your code here
+    return 0
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    print(result)
