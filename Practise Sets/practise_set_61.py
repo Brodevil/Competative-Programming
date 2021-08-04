8c6bcb9188ee5d4e67f9ef097d464a153b691694
Brodevil <brodevil89@gmail.com>
2021-08-04 19:23:06 +0530

    file for next problem Statement

---
+++ Practise Sets/practise_set_61.py
@@ -0,0 +1,21 @@
+# Python Practise 61 = Sales by Match (From hackerrank)
+
+"""
+Sales by Match | Problem Statment :
+
+
+Get the Problem Statement online on Hakerrank : https://www.hackerrank.com/challenges/sock-merchant/problem
+
+And get the solved solution in python here : |
+
+
+"""
+
+
+# Author = Abhinav
+# Date = 4 April 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  https://www.hackerrank.com/challenges/sock-merchant/problem
+
+
+# Solution :
