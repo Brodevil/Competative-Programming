a09aee2fa4fa5cff985dd4e66a5a44e3519ac1ea
Brodevil <brodevil89@gmail.com>
2021-08-05 19:03:33 +0530

    Initial commit

---
+++ Practise Sets/practise_set_62.py
@@ -0,0 +1,20 @@
+# Python Practise 62 = Drawing Book (From hackerrank)
+
+"""
+Drawing Book | Problem Statemnet :
+
+
+Get the Problem Statement online only on Hackerrank : https://www.hackerrank.com/challenges/drawing-book/problem
+
+And get the solved solution in python here :|
+
+"""
+
+
+# Author = Abhinav
+# Date = 5 April 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  https://www.hackerrank.com/challenges/drawing-book/problem
+
+
+# Solution :
