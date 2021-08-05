0d2d61923e2c5527c46199b104cfdfb56ad5539c
Brodevil <brodevil89@gmail.com>
2021-08-05 19:05:09 +0530

    Basic solution template by hackerrank

--- Practise Sets/practise_set_62.py
+++ Practise Sets/practise_set_62.py
@@ -18,3 +18,27 @@ And get the solved solution in python here :|
 
 
 # Solution :
+
+#!/bin/python3
+
+# Complete the 'pageCount' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER p
+
+
+def pageCount(n, p):
+    # Write your code here
+    return 0
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    p = int(input().strip())
+
+    result = pageCount(n, p)
+
+    print(result)
