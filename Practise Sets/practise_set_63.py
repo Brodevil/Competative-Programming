edc8b1664b5e83949ed8cba6c8205e62a98c2fd1
Brodevil <brodevil89@gmail.com>
2021-08-06 20:01:46 +0530

    Initial commit

---
+++ Practise Sets/practise_set_63.py
@@ -0,0 +1,22 @@
+# Python Practise 63 = Counting Valleys (From hackerrank)
+
+
+"""
+Counting Valleys | Problem Statment :
+
+
+Get the Problem Statment online on hackerrank : https://www.hackerrank.com/challenges/counting-valleys/problem
+
+And get the solved solution in Python3 here :|
+
+
+"""
+
+
+# Author = Abhinav
+# Date = 6 April 2021
+# Pourpose = Just for practise and imporving skills
+# Source =   https://www.hackerrank.com/challenges/counting-valleys/problem
+
+
+# Solution :
