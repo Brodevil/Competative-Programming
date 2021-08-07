1ffa465b1bb52b84d38dc7e028cab237116fb678
Brodevil <brodevil89@gmail.com>
2021-08-07 19:42:01 +0530

    Intial commit

---
+++ Practise Sets/practise_set_64.py
@@ -0,0 +1,19 @@
+# Python Practise 64 = Electronics Shop (From Hackerrank)
+
+"""
+Electronics Shop | Problem Statment :
+
+
+Get the Problem Statment on Hackerrank : https://www.hackerrank.com/challenges/electronics-shop/problem
+
+And Get the solved solution in python here :|
+
+"""
+
+
+# Author = Abhinav
+# Date = 6 April 2021
+# Pourpose = Just for practise and imporving skills
+# Source =   https://www.hackerrank.com/challenges/electronics-shop/problem
+
+# Solution :
