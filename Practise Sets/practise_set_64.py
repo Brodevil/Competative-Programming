2b75ec35115d75cca5945657023251c4e88df76f
Brodevil <brodevil89@gmail.com>
2021-08-07 19:44:36 +0530

    baisc solution template by hackerrank

--- Practise Sets/practise_set_64.py
+++ Practise Sets/practise_set_64.py
@@ -17,3 +17,31 @@ And Get the solved solution in python here :|
 # Source =   https://www.hackerrank.com/challenges/electronics-shop/problem
 
 # Solution :
+
+#!/bin/python3
+
+# Complete the getMoneySpent function below.
+
+
+def getMoneySpent(keyboards, drives, b):
+    return 0
+
+
+if __name__ == '__main__':
+    bnm = input().split()
+
+    b = int(bnm[0])
+
+    n = int(bnm[1])
+
+    m = int(bnm[2])
+
+    keyboards = list(map(int, input().rstrip().split()))
+
+    drives = list(map(int, input().rstrip().split()))
+    
+    # The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+    
+    moneySpent = getMoneySpent(keyboards, drives, b)
+
+    print(moneySpent)
