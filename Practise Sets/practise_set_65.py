8c510c30cb0af03d6184856c1a9eb88b0cf476f1
Brodevil <brodevil89@gmail.com>
2021-08-07 19:59:32 +0530

    Intial commit

---
+++ Practise Sets/practise_set_65.py
@@ -0,0 +1,20 @@
+# Python Practise 65 = Cats and a Mouss (From hackerrank) 
+
+
+"""
+Cats and a Mouse | Problem Statment :
+
+
+Get the Problem Statment on Hackerrank : https://www.hackerrank.com/challenges/cats-and-a-mouse/problem
+
+And get the solved solution in python by Brodevil here :|
+
+"""
+
+# Author = Abhinav
+# Date = 7 April 2021
+# Pourpose = Just for practise and imporving skills
+# Source =   https://www.hackerrank.com/challenges/cats-and-a-mouse/problem
+
+# Solution :
+
