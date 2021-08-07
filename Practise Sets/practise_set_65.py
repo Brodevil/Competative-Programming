df2bba002b044d2a24dcd9474c2ac7acd50abb05
Brodevil <brodevil89@gmail.com>
2021-08-07 20:03:29 +0530

    Basic solution tempalte by Hackerrank

--- Practise Sets/practise_set_65.py
+++ Practise Sets/practise_set_65.py
@@ -18,3 +18,25 @@ And get the solved solution in python by Brodevil here :|
 
 # Solution :
 
+# Complete the catAndMouse function below.
+
+
+def catAndMouse(x, y, z):
+    return 0
+
+
+if __name__ == '__main__':
+    q = int(input())
+
+    for q_itr in range(q):
+        xyz = input().split()
+
+        x = int(xyz[0])
+
+        y = int(xyz[1])
+
+        z = int(xyz[2])
+
+        result = catAndMouse(x, y, z)
+
+        print(result)
