81b0ac8262d33d1f99295ef1c9c8568b2cd6887c
Brodevil <brodevil89@gmail.com>
2021-08-08 19:04:25 +0530

    Intial commit

---
+++ Practise Sets/practise_set_66.py
@@ -0,0 +1,21 @@
+# Python practise 66 = Forming a Magic Square (From hackerrank)
+
+"""
+Forming a Magic Square  |  Problem Statment :
+
+
+Get the Problem Statment on hackerrank : https://www.hackerrank.com/challenges/magic-square-forming/problem
+
+And get the solved solution in python by Brodevil here :|
+
+
+"""
+
+
+# Author = Abhinav
+# Date = 8 August 2021
+# Pourpose = Just for practise and imporving skills
+# Source =   https://www.hackerrank.com/challenges/magic-square-forming/problem
+
+# Solution :
+
