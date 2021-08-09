9522414fcb77d4550cbf318c3cb37e4102d502ce
Brodevil <brodevil89@gmail.com>
2021-08-09 20:02:35 +0530

    Intial commit

---
+++ Practise Sets/practise_set_67.py
@@ -0,0 +1,21 @@
+# Python practise 67 = Picking Numbers (From hackerank)
+
+"""
+Picking Numbers | Problem Statement :
+
+
+Get the Problem Statment on hackerrank : https://www.hackerrank.com/challenges/picking-numbers/problem
+
+And get the sovled solution in python by Brodevil here :|
+
+
+"""
+
+
+# Author = Abhinav
+# Date = 9 August 2021
+# Pourpose = Just for practise and imporving python skills
+# Source =   https://www.hackerrank.com/challenges/picking-numbers/problem
+
+# Solution :
+
