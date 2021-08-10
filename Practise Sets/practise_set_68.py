72823323eab2b19caaca5a010cefb8625d544b69
Brodevil <brodevil89@gmail.com>
2021-08-10 20:25:41 +0530

    question from codeforces

---
+++ Practise Sets/practise_set_68.py
@@ -0,0 +1,21 @@
+# Python Practise 68 =  Ezzat and Two Subsequences (From codeforces)
+
+"""
+Ezzat and Two Subsequences | Problem Statment :
+
+
+Get the Problem Statment on Code Forces : https://codeforces.com/problemset/problem/1557/A
+
+And get the solved solution in python here :|
+
+"""
+
+
+# Author = Abhinav
+# Date = 10 August 2021
+# Pourpose = Just for practise and imporving python skills
+# Source =   https://codeforces.com/problemset/problem/1557/A
+
+
+# Solution :
+
