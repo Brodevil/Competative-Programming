dd32555a2d494b95847b9e9324b74d04f0c35e8a
Brodevil <brodevil89@gmail.com>
2021-08-11 19:13:08 +0530

    Still remaining to solve

--- Practise Sets/practise_set_68.py
+++ Practise Sets/practise_set_68.py
@@ -1,7 +1,7 @@
 # Python Practise 68 =  Ezzat and Two Subsequences (From codeforces)
 
 """
-Ezzat and Two Subsequences | Problem Statment :
+Ezzat and Two Subsequences | Problem Statment (A):
 
 
 Get the Problem Statment on Code Forces : https://codeforces.com/problemset/problem/1557/A
@@ -19,3 +19,24 @@ And get the solved solution in python here :|
 
 # Solution :
 
+from itertools import permutations
+
+
+def solve(array: list):
+    largest = 0
+    for _ in range(len(array)):
+        currect_array = array.copy()
+        currect_array.remove(array[_])
+        ans = array[_] + (sum(currect_array) / len(currect_array))
+        if ans > largest:
+            largest = ans
+    
+    return largest
+
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        input()
+        array = list(map(int, input().split()))
+        result = solve(array)
+        print(result)
