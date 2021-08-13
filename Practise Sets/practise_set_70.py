7798e9256235e9abd8f8f5a973e84b0757cafc51
Brodevil <brodevil89@gmail.com>
2021-08-13 19:47:15 +0530

    Initial commit

--- Practise Sets/practise_set_70.py
+++ Practise Sets/practise_set_70.py
@@ -32,8 +32,6 @@ def hurdleRace(k, height):
 
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     first_multiple_input = input().rstrip().split()
 
     n = int(first_multiple_input[0])
