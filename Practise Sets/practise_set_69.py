01433ad16a1f458ec40a98c59a0f7e47a4cc4cbf
Brodevil <brodevil89@gmail.com>
2021-08-13 18:39:53 +0530

    In Process to solve

--- Practise Sets/practise_set_69.py
+++ Practise Sets/practise_set_69.py
@@ -28,22 +28,31 @@ And get the solved solution in python by Brodevil here :|
 #  1. INTEGER_ARRAY ranked
 #  2. INTEGER_ARRAY player
 
-def index(iterator, element):
+def index(iterator, element) -> int:
     for index, value in enumerate(iterator):
         if value == element:
             return len(iterator) - (index)
 
+
+def diff(iterator) -> list:
+    uniqe = list()
+    for _ in iterator:
+        if _ not in uniqe:
+            uniqe.append(_)
+    
+    return uniqe
+
+
 def climbingLeaderboard(ranked: list, player: list) -> list:
-    ranks = {index(set(sorted(ranked)), _) : _ for _ in ranked}
+    ranks = {index(diff(sorted(ranked)), _) : _ for _ in ranked}
     result = list()
 
-    for _ in player:
-        ranked.appne(_)
-
-
-
+    # for _ in player:
+    #     ranked.append(_)
+    print(ranks)
 
     return result
+
 
 if __name__ == '__main__':
     ranked_count = int(input().strip())
