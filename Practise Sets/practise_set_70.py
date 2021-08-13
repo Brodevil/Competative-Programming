93c1967c099e08599a79aed5d8eedf97a2e0c574
Brodevil <brodevil89@gmail.com>
2021-08-13 19:45:22 +0530

    Basic solution template by hackerraank

--- Practise Sets/practise_set_70.py
+++ Practise Sets/practise_set_70.py
@@ -18,3 +18,30 @@ And get the solution in python here :|
 
 # Solution :
 
+# Complete the 'hurdleRace' function below.
+
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY height
+
+
+def hurdleRace(k, height):
+    # Write your code here
+    return 0
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    height = list(map(int, input().rstrip().split()))
+
+    result = hurdleRace(k, height)
+
+    print(result)
