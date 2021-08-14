2b0238ba725e148dc7ba2c4330968c6ee1c24a49
Brodevil <brodevil89@gmail.com>
2021-08-14 19:00:56 +0530

    Intial commit

---
+++ Practise Sets/practise_set_71.py
@@ -0,0 +1,20 @@
+# Python Practise 71 =  Utopian Tree (From Hackerrank)
+
+"""
+Utopian Tree | Problem Statement :
+
+
+Get the Problem Statement on hackerrank : https://www.hackerrank.com/challenges/utopian-tree/problem
+
+And get the solved solution in python by brodevil here :|
+
+
+"""
+
+# Author = Abhinav
+# Date = 14 August 2021
+# Pourpose = Just for practise and imporving python skills
+# Source =  https://www.hackerrank.com/challenges/utopian-tree/problem
+
+
+# Solution :
