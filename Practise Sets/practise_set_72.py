46b751b59f76f4f59a3ec9e41d621107667adc71
Brodevil <brodevil89@gmail.com>
2021-08-14 19:07:20 +0530

    Intial commit

---
+++ Practise Sets/practise_set_72.py
@@ -0,0 +1,20 @@
+# Python Practise 72 = A. PizzaForces (From CodeForces)
+
+"""
+A. PizzaForces | Problem Statement :
+
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1555/A
+
+And get the solved solution in python by brodevil here :|
+
+"""
+
+
+# Author = Abhinav
+# Date = 14 August 2021
+# Pourpose = Just for practise and imporving python skills
+# Source =  [CodeForces](https://codeforces.com/problemset/problem/1555/A)
+
+
+# Solution :
