ece56cc14cf0fe1350d8a3eb906205505b15ea56
Brodevil <brodevil89@gmail.com>
2021-08-18 20:18:19 +0530

    Intial commti

--- Algo-Questions/practise_set_74.py
+++ Algo-Questions/practise_set_74.py
@@ -4,3 +4,16 @@
 Viral Advertising | Problem Statment :
 
 
+Get the Problem Statment on Hackerrank : https://www.hackerrank.com/challenges/strange-advertising/problem
+
+And get the solved solution in python by Brodevil here :|
+
+"""
+
+# Author = Abhinav
+# Date = 18 August 2021 
+# Pourpose = Just for practise and imporving python skills
+# Source =  [Hackerrnak](https://www.hackerrank.com/challenges/strange-advertising/problem)
+
+
+# Solution :
