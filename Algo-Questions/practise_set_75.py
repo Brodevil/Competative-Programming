2cc7c7f7c6765b90da76b14caab4aa7bf3bf2ee0
Brodevil <brodevil89@gmail.com>
2021-08-20 19:57:58 +0530

    Inital commit

---
+++ Algo-Questions/practise_set_75.py
@@ -0,0 +1,20 @@
+# Algorith Question 75 =  Save the Prisoner! (From Hackerrank)
+
+"""
+Save the Prisoner! | Problem Statement :
+
+
+Get the Problem Statment on HackerRank : https://www.hackerrank.com/challenges/save-the-prisoner/problem
+
+And get the solved solution in python by Brodevil here :|
+
+
+"""
+
+# Author = Abhinav
+# Date = 20 August 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [Hackerrnak](https://www.hackerrank.com/challenges/save-the-prisoner/problem)
+
+
+# Solution :
