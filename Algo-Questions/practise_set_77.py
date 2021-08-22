621ccdbf721716520e760e78fbde3b968860796a
Brodevil <brodevil89@gmail.com>
2021-08-22 19:58:40 +0530

    Initial Commit

--- Algo-Questions/practise_set_77.py
+++ Algo-Questions/practise_set_77.py
@@ -9,3 +9,32 @@ Get the Problem Statement on Hackerrank : https://www.hackerrank.com/challenges/
 And get the solution in python by Brodevil here :|
 
 """
+
+# Author = Abhinav
+# Date = 21 August 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [Hackerrnak](https://www.hackerrank.com/challenges/permutation-equation/problem)
+
+
+# Solution :
+
+
+# Complete the 'permutationEquation' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY p as parameter.
+
+
+def permutationEquation(p: list) -> list:
+    # Write your code here
+    return 0
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    p = list(map(int, input().rstrip().split()))
+
+    result = permutationEquation(p)
+
+    print(result)
