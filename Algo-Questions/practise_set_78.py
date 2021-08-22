4f45cd98b0f3ef81d9a82265d76291853972da82
Brodevil <brodevil89@gmail.com>
2021-08-22 21:18:09 +0530

    Intial Commit

---
+++ Algo-Questions/practise_set_78.py
@@ -0,0 +1,19 @@
+# Algorithms Question 78 = Jumping on the Clouds: Revisited (From HackerRank)
+
+"""
+Jumping on the Clouds: Revisited | Problem Statment :
+
+
+Get the Problem Statement on Hackerrank : https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem
+
+And get the solved solution in python By Brodevil here :|
+
+"""
+
+# Author = Abhinav
+# Date = 22 August 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [Hackerrank](https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem)
+
+
+# Solution :
