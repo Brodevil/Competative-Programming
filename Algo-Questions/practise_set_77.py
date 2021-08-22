4562d32cab31c3a2cbee2d18c064837261696b7e
Brodevil <brodevil89@gmail.com>
2021-08-22 19:52:27 +0530

    Initial Commit

---
+++ Algo-Questions/practise_set_77.py
@@ -0,0 +1,11 @@
+# Algorithm Question 77 = Sequence Equation (From HackerRank)
+
+"""
+Sequence Equation | Problem Statement :
+
+
+Get the Problem Statement on Hackerrank : https://www.hackerrank.com/challenges/permutation-equation/problem
+
+And get the solution in python by Brodevil here :|
+
+"""
