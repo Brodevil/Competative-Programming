afdf9a89513e8e70bf30023c9e34a78293c8e01a
Brodevil <brodevil89@gmail.com>
2021-08-23 18:41:19 +0530

    Intial Commit

--- Algo-Questions/practise_set_78.py
+++ Algo-Questions/practise_set_78.py
@@ -15,5 +15,22 @@ And get the solved solution in python By Brodevil here :|
 # Pourpose = Just for practise and imporving skills
 # Source =  [Hackerrank](https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem)
 
-
 # Solution :
+
+
+def jumpingOnClouds(c, k):
+    return 0
+
+
+if __name__ == '__main__':
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c, k)
+
+    print(result)
