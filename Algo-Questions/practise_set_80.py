b9737cc53c47dd7c80b04dcf598f4a1ba273df1a
Brodevil <brodevil89@gmail.com>
2021-08-25 19:01:34 +0530

    Initial Commit

--- Algo-Questions/practise_set_80.py
+++ Algo-Questions/practise_set_80.py
@@ -9,3 +9,24 @@ Get the Problem Statment on HackerRank : https://www.hackerrank.com/challenges/e
 And get the Solution in python By Brodevil, here :|
 
 """
+
+# Author = Abhinav
+# Date = 25 August 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [Hackerrank](https://www.hackerrank.com/challenges/extra-long-factorials/problem)
+
+# Solution :
+
+# Complete the 'extraLongFactorials' function below.
+# The function accepts INTEGER n as parameter.
+
+
+def extraLongFactorials(n: int) -> int:
+    # Write your code here
+    return 0
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    result = extraLongFactorials(n)
+    print(result)
