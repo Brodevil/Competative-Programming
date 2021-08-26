c10bc78d2e5d24fb697845a30337670347963ff7
Brodevil <brodevil89@gmail.com>
2021-08-26 18:55:43 +0530

    Basic solution By HackerRank

--- Algo-Questions/practise_set_81.py
+++ Algo-Questions/practise_set_81.py
@@ -9,3 +9,36 @@ Get the problem Statement on HackerRank : https://www.hackerrank.com/challenges/
 And get the solved solution in python By Brodevil here :|
 
 """
+
+# Author = Abhinav
+# Date = 26 August 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [Hackerrank](https://www.hackerrank.com/challenges/append-and-delete/problem)
+
+# Solution :
+
+
+# Complete the 'appendAndDelete' function below.
+
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. STRING t
+#  3. INTEGER k
+
+
+def appendAndDelete(s: str, t: str, k: int) -> str:
+    # Write your code here
+    return "YES"
+
+
+if __name__ == '__main__':
+    s = input()
+
+    t = input()
+
+    k = int(input().strip())
+
+    result = appendAndDelete(s, t, k)
+
+    print(result)
