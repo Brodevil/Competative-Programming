9feba8db7d7402e6647e005cfc6e68ecfb673dea
Brodevil <brodevil89@gmail.com>
2021-08-26 18:53:27 +0530

    Intial Commit

---
+++ Algo-Questions/practise_set_81.py
@@ -0,0 +1,11 @@
+# Algorithm Question 81 = Append and Delete (From HakcerRank)
+
+"""
+Append and Delete | Problem Statement :
+
+
+Get the problem Statement on HackerRank : https://www.hackerrank.com/challenges/append-and-delete/problem
+
+And get the solved solution in python By Brodevil here :|
+
+"""
