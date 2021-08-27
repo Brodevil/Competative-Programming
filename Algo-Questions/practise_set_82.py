ec46b16de96aa9ff0335c7ff858536001a545b46
Brodevil <brodevil89@gmail.com>
2021-08-27 19:48:32 +0530

    Intial Commit

--- Algo-Questions/practise_set_82.py
+++ Algo-Questions/practise_set_82.py
@@ -16,3 +16,30 @@ And get the solved solution in python by Brodevil here :|
 # Source =  [Hackerrank](https://www.hackerrank.com/challenges/sherlock-and-squares/problem)
 
 # Solution :
+
+# Complete the 'squares' function below.
+
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER a
+#  2. INTEGER b
+
+
+def squares(a: int, b: int) -> int:
+    # Write your code here
+    return 0
+
+
+if __name__ == '__main__':
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        first_multiple_input = input().rstrip().split()
+
+        a = int(first_multiple_input[0])
+
+        b = int(first_multiple_input[1])
+
+        result = squares(a, b)
+
+        print(result)
