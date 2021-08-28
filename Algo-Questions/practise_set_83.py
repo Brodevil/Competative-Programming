e9462f68bd1cfc2287c3be9b6c9c16199b75855d
Brodevil <brodevil89@gmail.com>
2021-08-28 19:40:01 +0530

    Intial commit

---
+++ Algo-Questions/practise_set_83.py
@@ -0,0 +1,12 @@
+# Algorithm Question 83 = Library Fine (From HakcerRank)
+
+"""
+Library Fine | Problem Statement :
+
+
+Get the Problem Statement on HackerRank : https://www.hackerrank.com/challenges/library-fine/problem
+
+And get the solved solutiog in python by Brodevil, here :|
+
+"""
+
