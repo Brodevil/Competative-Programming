aa0a3668cf0ec5f90f5e5cd39882225a91a78029
Brodevil <brodevil89@gmail.com>
2021-08-28 19:43:29 +0530

    basic input format by HackerRank

--- Algo-Questions/practise_set_83.py
+++ Algo-Questions/practise_set_83.py
@@ -10,3 +10,46 @@ And get the solved solutiog in python by Brodevil, here :|
 
 """
 
+# Author = Abhinav
+# Date = 28 August 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [Hackerrank](https://www.hackerrank.com/challenges/library-fine/problem)
+
+# Solution :
+
+# Complete the 'libraryFine' function below.
+
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER d1
+#  2. INTEGER m1
+#  3. INTEGER y1
+#  4. INTEGER d2
+#  5. INTEGER m2
+#  6. INTEGER y2
+
+
+def libraryFine(d1: int, m1: int, y1: int, d2: int, m2: int, y2: int) -> int:
+    return 0 
+
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    d1 = int(first_multiple_input[0])
+
+    m1 = int(first_multiple_input[1])
+
+    y1 = int(first_multiple_input[2])
+
+    second_multiple_input = input().rstrip().split()
+
+    d2 = int(second_multiple_input[0])
+
+    m2 = int(second_multiple_input[1])
+
+    y2 = int(second_multiple_input[2])
+
+    result = libraryFine(d1, m1, y1, d2, m2, y2)
+
+    print(result)
