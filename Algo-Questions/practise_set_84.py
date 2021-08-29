46fdda980ffd5a69169a75de073b5edb0583e30e
Brodevil <brodevil89@gmail.com>
2021-08-29 19:04:32 +0530

    Intial Commit

---
+++ Algo-Questions/practise_set_84.py
@@ -0,0 +1,39 @@
+# Algorithms Question 84 = Cut the sticks (From HackerRank)
+
+"""
+Cut the sticks | Problem Statement :
+
+
+Get the Problem Statement on HackerRank : https://www.hackerrank.com/challenges/cut-the-sticks/problem
+
+And get the solved solution in python By Brodevil, here :|
+
+"""
+
+# Author = Abhinav
+# Date = 29 August 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [Hackerrank](https://www.hackerrank.com/challenges/cut-the-sticks/problem)
+
+
+# Solution :
+
+# Complete the 'cutTheSticks' function below.
+
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+
+
+def cutTheSticks(arr: list) -> list:
+    # Write your code here
+    return list
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = cutTheSticks(arr)
+
+    print(result)
