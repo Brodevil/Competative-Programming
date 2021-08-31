26419905f6b1c186a7c6bf7a4c4ad672effc6576
Brodevil <brodevil89@gmail.com>
2021-08-31 19:29:23 +0530

    Intial Commit

--- Algo-Questions/practise_set_86.py
+++ Algo-Questions/practise_set_86.py
@@ -17,3 +17,25 @@ And get the solved solution in python By Brodevil here :|
 
 
 # Solution :
+
+# Complete the 'repeatedString' function below.
+
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. LONG_INTEGER n
+
+
+def repeatedString(s: str, n: int) -> int:
+    # Write your code here
+    return 0
+
+
+if __name__ == '__main__':
+    s = input()
+
+    n = int(input().strip())
+
+    result = repeatedString(s, n)
+
+    print(result)
