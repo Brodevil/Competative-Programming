74dbbe6ac1e585847526fcfa0f1186b70aa04428
Brodevil <brodevil89@gmail.com>
2021-08-31 19:21:20 +0530

    solved the Questinon

--- Algo-Questions/practise_set_85.py
+++ Algo-Questions/practise_set_85.py
@@ -27,7 +27,18 @@ And get the solved solution in python by Brodevil here :|
 
 
 def nonDivisibleSubset(k: int, s: list) -> int:
-    return 0
+    counts = [0] * k
+    for number in s:
+        counts[number % k] += 1
+
+    count = min(counts[0], 1)
+    for i in range(1, k//2+1):
+        if i != k - i:
+            count += max(counts[i], counts[k-i])
+    if k % 2 == 0: 
+        count += 1
+
+    return count
 
 
 if __name__ == '__main__':
