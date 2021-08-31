69fdc7fccd3617c5141b8015043be99d59e0bf0d
Brodevil <brodevil89@gmail.com>
2021-08-31 19:26:19 +0530

    Intial Commit

---
+++ Algo-Questions/practise_set_86.py
@@ -0,0 +1,19 @@
+# Algorith Question 86 = Repeated String (From HackerRank)
+
+"""
+Repeated String | Problem Statement :
+
+Get the Problem Statement on Hackerrank : https://www.hackerrank.com/challenges/repeated-string/problem
+
+And get the solved solution in python By Brodevil here :|
+
+"""
+
+
+# Author = Abhinav
+# Date = 31 August 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [Hackerrank](https://www.hackerrank.com/challenges/repeated-string/problem)
+
+
+# Solution :
