ebf5b27110153b36bfb2c5f7cde5e902657b9f25
Brodevil <brodevil89@gmail.com>
2021-09-01 20:16:25 +0530

    Intial Commit

---
+++ Algo-Questions/practise_set_87.py
@@ -0,0 +1,39 @@
+# Algorithm Question 87 = Jumping on the Clouds (From HackerRank)
+
+"""
+Jumping on the Clouds | Problem Statement : 
+
+
+Get the Problem Statement on HackerRank : https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
+
+And get the solved solution in python By Brodevil, here :|
+
+"""
+
+# Author = Abhinav
+# Date = 1 September 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [Hackerrank](https://www.hackerrank.com/challenges/repeated-string/problem)
+
+
+# Solution :
+
+# Complete the 'jumpingOnClouds' function below.
+
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY c as parameter.
+
+
+def jumpingOnClouds(c: list) -> int:
+    # Write your code here
+    return 0 
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    print(result)
