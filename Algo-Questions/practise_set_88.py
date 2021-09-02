1c5ad2f6abd50ff0175ae14373e9115f6b21ea0a
Brodevil <brodevil89@gmail.com>
2021-09-02 19:37:23 +0530

    Intial Commit

---
+++ Algo-Questions/practise_set_88.py
@@ -0,0 +1,39 @@
+# Algorith Question 88 = Equalize the Array (From HackerRank)
+
+"""
+Equalize the Array |
+
+
+Get the Problem Statement : https://www.hackerrank.com/challenges/equality-in-a-array/problem
+
+And get the solved solution in python by Brodevil, here :
+
+"""
+
+# Author = Abhinav
+# Date = 2 September 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [Hackerrank](https://www.hackerrank.com/challenges/equality-in-a-array/problem)
+
+
+# Solution :
+
+# Complete the 'equalizeArray' function below.
+
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+
+
+def equalizeArray(arr: list) -> int:
+    # Write your code here
+    return 0
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = equalizeArray(arr)
+
+    print(result)
