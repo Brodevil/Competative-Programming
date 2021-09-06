8ee1088f4c414b89fbdd20d0cfc46c0253db5ec3
Brodevil <brodevil89@gmail.com>
2021-09-06 19:30:11 +0530

    Intial commit

---
+++ Algo-Questions/practise_set_92.py
@@ -0,0 +1,19 @@
+# Algorithm Question 92  =  Organizing Containers of Balls  (From HackerRank)
+
+"""
+Organizing Containers of Balls |
+
+
+Get the Problem Statement On HackerRank : https://www.hackerrank.com/challenges/organizing-containers-of-balls/problem
+
+And get the solved solution in python by Brodevil, here :|
+
+"""
+
+# Author = Abhinav
+# Date = 6 September 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [Hackerrank](https://www.hackerrank.com/challenges/taum-and-bday/problem)
+
+
+# Solution :
