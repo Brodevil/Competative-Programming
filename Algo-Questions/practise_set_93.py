199cf98b62be039ce0e29b02f9a6275d84d56769
Brodevil <brodevil89@gmail.com>
2021-09-07 19:12:58 +0530

    Initial commit

---
+++ Algo-Questions/practise_set_93.py
@@ -0,0 +1,18 @@
+# Algorithm Question 93  =  A. Domino Disaster (From CodeForces)
+
+"""
+A. Domino Disaster |
+
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1567/A
+
+And get the solved solution in python by Brodevil, here :|
+
+"""
+
+# Author = Abhinav
+# Date = 7 September 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeForces](https://codeforces.com/problemset/problem/1567/A)
+
+# Solution :
