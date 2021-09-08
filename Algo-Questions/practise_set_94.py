8365ab8c146add2ad97a220403f66585ce3bf59a
Brodevil <brodevil89@gmail.com>
2021-09-08 20:29:14 +0530

    Intial commit

---
+++ Algo-Questions/practise_set_94.py
@@ -0,0 +1,18 @@
+# Algorithm Question 94  =  B. MEXor Mixup (From CodeFoces)
+
+"""
+B. MEXor Mixup | 
+
+
+Get the Problem Statement on CodeFroces : https://codeforces.com/problemset/problem/1567/B
+
+And get the solved solution in python by Brodevil, here :|
+
+"""
+
+# Author = Abhinav
+# Date = 8 September 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeForces](https://codeforces.com/problemset/problem/1567/B)
+
+# Solution :
