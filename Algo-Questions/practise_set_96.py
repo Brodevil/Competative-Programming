0e54d2faffb6f8db4445a37c8c01529282fd6009
Brodevil <brodevil89@gmail.com>
2021-09-10 20:50:45 +0530

    Intial Commit

---
+++ Algo-Questions/practise_set_96.py
@@ -0,0 +1,18 @@
+# Algorithm Question 96  =  Designer PDF Viewer
+
+"""
+Designer PDF Viewer |
+
+
+Get the Problem Statement on HackerRank : https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
+
+And get the solved solution in python by Brodevil, here :|
+
+"""
+
+# Author = Abhinav
+# Date = 9 September 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/designer-pdf-viewer/problem)
+
+# Solution :
