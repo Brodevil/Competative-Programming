2e6074151f75198a3964d6e08992c87ee199cba4
Brodevil <brodevil89@gmail.com>
2021-09-12 19:39:10 +0530

    Intial commit

---
+++ Algo-Questions/practise_set_98.py
@@ -0,0 +1,18 @@
+# Algorithm Question 98  =  A. Balanced Substring (From CodeForces)
+
+"""
+A. Balanced Substring |
+
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1569/A
+
+And get the solved solution in python by Brodevil, here :|
+
+"""
+
+# Author = Abhinav
+# Date = 12 September 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeFroces](https://codeforces.com/problemset/problem/1569/A)
+
+# Solution :
