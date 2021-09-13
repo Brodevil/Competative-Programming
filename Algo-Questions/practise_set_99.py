25814900734bbe76e2566b2c6869ddee6d6340bc
Brodevil <brodevil89@gmail.com>
2021-09-13 19:21:21 +0530

    Intial commit

---
+++ Algo-Questions/practise_set_99.py
@@ -0,0 +1,18 @@
+# Algorithm Question 99  =  A. Mocha and Math
+
+"""
+A. Mocha and Math |
+
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1559/A
+
+And get the solved solution in python by Brodevil, here :|
+
+"""
+
+# Author = Abhinav
+# Date = 13 September 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeFroces](https://codeforces.com/problemset/problem/1559/A)
+
+# Solution :
