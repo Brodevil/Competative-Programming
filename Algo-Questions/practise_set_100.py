2bbaa5f03eec86146f69fafc04747ee7cfc8116e
Brodevil <brodevil89@gmail.com>
2021-09-14 19:31:52 +0530

    Intial commit

---
+++ Algo-Questions/practise_set_100.py
@@ -0,0 +1,18 @@
+# Algorithm Question 100  =  Encryption (From HackerRank)
+
+"""
+Encryption | 
+
+
+Get the Probelm Statement on HackerRank : https://www.hackerrank.com/challenges/encryption/problem
+
+And get the solved solution in python by Brodevil, here :|
+
+"""
+
+# Author = Abhinav
+# Date = 14 September 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/encryption/problem)
+
+# Solution :
