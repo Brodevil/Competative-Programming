2c2c5bfd0036bfc096c96489b2e0c9d2108fc56b
Brodevil <brodevil89@gmail.com>
2021-09-15 20:25:28 +0530

    Intial commit

---
+++ Algo-Questions/practise_set_101.py
@@ -0,0 +1,18 @@
+# Algorithm Question 101  =  Bigger is Greater
+
+"""
+Bigger is Greater |
+
+
+Get the Problem Statement on HackerRank : https://www.hackerrank.com/challenges/bigger-is-greater/problem
+
+And get the solved solution in python, below :|
+
+"""
+
+# Author = Abhinav
+# Date = 15 September 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/bigger-is-greater/problem)
+
+# Solution :
