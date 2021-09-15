391420fa578ed5f43686b3f441acae1b8f95d851
Brodevil <brodevil89@gmail.com>
2021-09-15 20:17:29 +0530

    1st attempt, most of the test cases are passed

--- Algo-Questions/practise_set_100.py
+++ Algo-Questions/practise_set_100.py
@@ -16,3 +16,29 @@ And get the solved solution in python by Brodevil, here :|
 # Source =  [HackerRank](https://www.hackerrank.com/challenges/encryption/problem)
 
 # Solution :
+
+# Complete the 'encryption' function below.
+
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+
+from math import sqrt
+
+
+def encryption(s: str) -> str:
+    l = int(sqrt(len(s)))
+    new = ''
+    for _ in range(l):
+        for i in range(_, len(s), l+1):
+            new += s[i]
+
+        new += " "
+    for _ in range(l, len(s), l+1):
+        new += s[_]
+    return new
+
+
+if __name__ == '__main__':
+    s = input()
+    result = encryption(s)
+    print(result)
