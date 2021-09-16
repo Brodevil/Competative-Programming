529eb4da49b896a32b14bd01324e2d12e357888a
Brodevil <brodevil89@gmail.com>
2021-09-16 20:17:44 +0530

    Intial commit

---
+++ Algo-Questions/practise_set_102.py
@@ -0,0 +1,18 @@
+# Algorithm Question 102  =  Modified Kaprekar Numbers
+
+"""
+Modified Kaprekar Numbers |
+
+
+Get the Problem Statement on HackerRank : https://www.hackerrank.com/challenges/kaprekar-numbers/problem
+
+And get the solved solution in python, here :|
+
+"""
+
+# Author = Abhinav
+# Date = 16 September 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/kaprekar-numbers/problem)
+
+# Solution :
