d47205da5ae92c8b12afac8dc14e3b099030913c
Brodevil <brodevil89@gmail.com>
2021-09-17 19:29:01 +0530

    first attempt, let see wheather time expcreed or not XD:

--- Algo-Questions/practise_set_102.py
+++ Algo-Questions/practise_set_102.py
@@ -16,3 +16,23 @@ And get the solved solution in python, here :|
 # Source =  [HackerRank](https://www.hackerrank.com/challenges/kaprekar-numbers/problem)
 
 # Solution :
+
+if __name__ == "__main__":
+    p, q = int(input()), int(input())
+    results = list()
+    for _ in range(p, q+1):
+        sq = _*_
+        d = len(str(sq))
+        temp = list(str(sq))
+        l, r = ''.join(temp[:d//2]), int(''.join(temp[d//2:]))
+
+        if not len(l):
+            l += "0"
+            l = int(l)
+        else:
+            l = int(l)
+        
+        if l + r == _:
+            results.append(_)
+        
+    print(*results)
