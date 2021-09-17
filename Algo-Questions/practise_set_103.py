1f4a7fdb122f5d70708c141e9bd0afd867209abd
Brodevil <brodevil89@gmail.com>
2021-09-17 19:34:00 +0530

    Initial commit

---
+++ Algo-Questions/practise_set_103.py
@@ -0,0 +1,19 @@
+# Algorithm Question 103  =  Beautiful Triplets (From HackerRank)
+
+
+"""
+Beautiful Triplets |
+
+
+Get the Problem Statement on HackerRank : https://www.hackerrank.com/challenges/beautiful-triplets/problem
+
+And get the solved solution in python here :|
+
+"""
+
+# Author = Abhinav
+# Date = 17 September 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/beautiful-triplets/problem)
+
+# Solution :
