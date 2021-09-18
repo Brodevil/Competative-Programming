70a1f802721fefc9273e9dea1a85a2822dd25816
Brodevil <brodevil89@gmail.com>
2021-09-18 19:45:36 +0530

    Intial commit

---
+++ Algo-Questions/practise_set_104.py
@@ -0,0 +1,18 @@
+# Algorithm Practise 104  =  Minimum Distances (From HackerRank)
+
+"""
+Minimum Distances |
+
+
+Get the Problem Statement on HackerRank : https://www.hackerrank.com/challenges/minimum-distances/problem
+
+And get the solved solution in python, here:|
+
+"""
+
+# Author = Abhinav
+# Date = 18 September 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/minimum-distances/problem)
+
+# Solution :
