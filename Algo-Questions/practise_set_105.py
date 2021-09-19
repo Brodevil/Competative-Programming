5377d720860923f0c7757be440f0ffc5b422d5a7
Brodevil <brodevil89@gmail.com>
2021-09-19 19:17:12 +0530

    Intial Commit

---
+++ Algo-Questions/practise_set_105.py
@@ -0,0 +1,18 @@
+# Algorithm Questino 105  =  Halloween Sale (From HackerRank)
+
+"""
+Halloween Sale | 
+
+
+Get the Problem Statement on HackerRank : https://www.hackerrank.com/challenges/halloween-sale/problem
+
+And get the solved solution in python, here :|
+
+"""
+
+# Author = Abhinav
+# Date = 19 September 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/halloween-sale/problem)
+
+# Solution :
