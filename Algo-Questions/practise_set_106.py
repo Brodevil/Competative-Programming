17164570df6ff93fdafb756ef9b95df469e45544
Brodevil <brodevil89@gmail.com>
2021-09-20 20:36:30 +0530

    Intial commit:

--- Algo-Questions/practise_set_106.py
+++ Algo-Questions/practise_set_106.py
@@ -16,3 +16,14 @@ And get the solved soulution in python, here :|
 # Source =  [HackerRank](https://www.hackerrank.com/challenges/halloween-sale/problem)
 
 # Solution :
+
+
+def timeInWords(h: int, m: int) -> int:
+    # Write your code here
+    return 0
+
+
+if __name__ == '__main__':
+    h, m = int(input()), int(input())
+    result = timeInWords(h, m)
+    print(result)
