540bb4948aa4db34e974fdd32bd45a3e9a6cb0b9
Brodevil <brodevil89@gmail.com>
2021-09-20 20:19:05 +0530

    Intial commit

---
+++ Algo-Questions/practise_set_106.py
@@ -0,0 +1,18 @@
+# Algorithm Question 106  =  The Time in Words (From HackerRank)
+
+"""
+The Time in Words |
+
+
+Get the Problem Statement on HackerRank : https://www.hackerrank.com/challenges/the-time-in-words/problem
+
+And get the solved soulution in python, here :|
+
+"""
+
+# Author = Abhinav
+# Date = 19 September 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/halloween-sale/problem)
+
+# Solution :
