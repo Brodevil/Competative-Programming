291aeb2b93f0b01b62ae569420603b75271ee2ea
Brodevil <brodevil89@gmail.com>
2021-09-22 19:49:24 +0530

    Svae the Code in this possition

--- Algo-Questions/practise_set_107.py
+++ Algo-Questions/practise_set_107.py
@@ -16,3 +16,25 @@ And get the solved solution in python, here :|
 # Source =  [HackerRank](https://www.hackerrank.com/challenges/chocolate-feast/problem)
 
 # Solution :
+
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        n, c, m = map(int, input().split())
+        wrapper = [n // c]
+        remains = list()
+        while wrapper[-1] > 1:
+            if (wrapper[-1] / m) % 1 == 0:
+                temp = ((wrapper[-1] + sum(remains)) % m) 
+                if temp == 0:
+                    del remains[:]
+                    wrapper.append(temp)
+                else:
+                    wrapper.append((wrapper[-1] )// m)
+                    
+            else:
+                remains.append(wrapper[-1] % m)
+                wrapper.append(wrapper[-1]//m) 
+        
+        print(wrapper, remains)
+
