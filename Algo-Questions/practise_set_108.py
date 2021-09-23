a42ec9c1e99611a39313258a941717625106bb1e
Brodevil <brodevil89@gmail.com>
2021-09-23 19:32:23 +0530

    Intial commit

---
+++ Algo-Questions/practise_set_108.py
@@ -0,0 +1,11 @@
+# Algorithm Question 108  =  Service Lane (From HackerRank)
+
+"""
+Service Lane |
+
+
+Get the Problem Statement on HackerRank : https://www.hackerrank.com/challenges/service-lane/problem
+
+And get the python solution, here :|
+
+"""
