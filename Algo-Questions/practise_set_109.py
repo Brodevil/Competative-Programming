f0799c1f95722b8f6dccce9977b5b996a55645ec
Brodevil <brodevil89@gmail.com>
2021-09-25 19:16:47 +0530

    1st atempt

--- Algo-Questions/practise_set_109.py
+++ Algo-Questions/practise_set_109.py
@@ -16,3 +16,19 @@ And get the solved solution in python, here :|
 # Source =  [HackerRank](https://www.hackerrank.com/challenges/lisa-workbook/problem)
 
 # Solution :
+
+if __name__ == "__main__":
+    n, k = map(int, input().split())
+    arr = list(map(int, input().split()))
+    special = 0
+    page = 1
+    
+    for i in arr:
+        for _ in range(1, i+1):
+            if page == _:
+                special += 1
+            if _ > k:
+                page += 1
+        page += 1
+
+    print(special)
