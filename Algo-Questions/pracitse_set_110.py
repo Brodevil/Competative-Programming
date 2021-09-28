85af06d3d0953071d96db9bef7c44913b80eb828
Brodevil <brodevil89@gmail.com>
2021-09-28 18:39:51 +0530

    Intial Commit

--- Algo-Questions/pracitse_set_110.py
+++ Algo-Questions/pracitse_set_110.py
@@ -1,9 +1,9 @@
-# Algorithm Practise 110  =   (From HackerRank)
+# Algorithm Practise 110  =  Flatland Space Stations (From HackerRank)
 
 """
+Flatland Space Stations |
 
-
-Get the Problem Statement on HackerRank : 
+Get the Problem Statement on HackerRank : https://www.hackerrank.com/challenges/flatland-space-stations/problem
 
 And get the solved solution in python, here :|
 
@@ -12,6 +12,17 @@ And get the solved solution in python, here :|
 # Author = Abhinav
 # Date = 28 September 2021 
 # Pourpose = Just for practise and imporving skills
-# Source =  [HackerRank]()
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/flatland-space-stations/problem)
 
 # Solution :
+
+
+def flatlandSpaceStations(n, c):
+    return 0
+
+
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    c = list(map(int, input().rstrip().split()))
+    result = flatlandSpaceStations(n, c)
+    print(result)
