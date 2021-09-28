804eae5451f4d6dcfc0fbb0587127a125115ef5d
Brodevil <brodevil89@gmail.com>
2021-09-28 18:36:29 +0530

    Solved the QuestioN'

---
+++ Algo-Questions/pracitse_set_110.py
@@ -0,0 +1,17 @@
+# Algorithm Practise 110  =   (From HackerRank)
+
+"""
+
+
+Get the Problem Statement on HackerRank : 
+
+And get the solved solution in python, here :|
+
+"""
+
+# Author = Abhinav
+# Date = 28 September 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank]()
+
+# Solution :
--- Algo-Questions/practise_set_109.py
+++ Algo-Questions/practise_set_109.py
@@ -18,26 +18,27 @@ And get the solved solution in python, here :|
 # Solution :
 
 
-def foo(n):
-    global k, page, special
-    i = 1
-    for _ in range(1, int(n)+1):
-        if _ == page:
-            special += 1
-        if i-1 % k == 0:
-            page += 1
-            i = 1
-        print(_, page, special, i)
-        i += 1
+def lisas_workbook(n: int, k: int, a: list) -> int:
+    num_special = 0
+    cur_page = 1
+
+    for i in range(len(a)):
+        num_probs_in_chapter = a[i]
+        num_full_pages, leftover_probs = divmod(num_probs_in_chapter, k)
+
+        total_pages = num_full_pages + ( 1 if leftover_probs else 0 )
+        problems_in_chapter = iter(range(1, a[i]+1))
+
+        for _ in range(total_pages):
+            probs_on_page = [next(problems_in_chapter, None) for _ in range(k)]    
+            if cur_page in probs_on_page:
+                num_special += 1
+            cur_page += 1        
+    
+    return num_special
 
 
 if __name__ == "__main__":
     n, k = map(int, input().split())
     arr = list(map(int, input().split()))
-    page = 1
-    special = 0
-    for _ in arr:
-        foo(_)
-        page += 1
-
-    print(special)
+    print(lisas_workbook(n, k, arr))
