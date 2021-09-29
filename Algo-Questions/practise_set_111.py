02b70efd6bdd69c48c403de6320fbf16001642c2
Brodevil <brodevil89@gmail.com>
2021-09-29 20:41:59 +0530

    Intial Commit

---
+++ Algo-Questions/practise_set_111.py
@@ -0,0 +1,18 @@
+# Algorithm Question 111 =  A. Casimir's String Solitaire (From CodeForces)
+
+"""
+A. Casimir's String Solitaire |
+
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1579/A
+
+And get the solved solution in python, here :|
+
+"""
+
+# Author = Abhinav
+# Date = 29 September 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeForces](https://codeforces.com/problemset/problem/1579/A)
+
+# Solution :
