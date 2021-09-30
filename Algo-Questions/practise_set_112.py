c0238c274736d9a12a3bcdcf7c399142375b559e
Brodevil <brodevil89@gmail.com>
2021-09-30 19:17:13 +0530

    Intial Commit

---
+++ Algo-Questions/practise_set_112.py
@@ -0,0 +1,18 @@
+# Algorithm Question 112  =  A. CQXYM Count Permutations  (From CodeForces)
+
+"""
+A. CQXYM Count Permutations |
+
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1581/A
+
+And get solved solution, here :|
+
+"""
+
+# Author = Abhinav
+# Date = 30 September 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeForces](https://codeforces.com/problemset/problem/1581/A)
+
+# Solution :
