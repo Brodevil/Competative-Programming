4b3409bf6f70e688f29a20352ba3be814d566249
Brodevil <brodevil89@gmail.com>
2021-10-01 20:01:59 +0530

    Intial commit

---
+++ Algo-Questions/practise_set_113.py
@@ -0,0 +1,17 @@
+# Algorithm Question 113  =  Cavity Map (From HackerRank)
+
+"""
+Cavity Map |
+
+Get the Probblem Statement on HackerRank : https://www.hackerrank.com/challenges/cavity-map/problem
+
+And get the solved solution in python, here :}
+
+"""
+
+# Author = Abhinav
+# Date = 1 Output 2021 
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/cavity-map/problem)
+
+# Solution :
