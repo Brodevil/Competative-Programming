da2bc9f3683df3ac1ddc0b5e58d686fcecccb042
Brodevil <brodevil89@gmail.com>
2021-10-02 19:34:33 +0530

    Intial commit

--- Algo-Questions/practise_set_112.py
+++ Algo-Questions/practise_set_112.py
@@ -10,12 +10,7 @@ And get solved solution, here :|
 
 """
 
-# Author = Abhinav
-# Date = 30 September 2021 
-# Pourpose = Just for practise and imporving skills
-# Source =  [CodeForces](https://codeforces.com/problemset/problem/1573/A)
 
-# Solution :
 
 for s in [*open(0)][2::2]:
     print(sum(int(x)+(x>'0')for x in s[:-2])+int(s[-2]))
--- Algo-Questions/practise_set_113.py
+++ Algo-Questions/practise_set_113.py
@@ -10,8 +10,32 @@ And get the solved solution in python, here :}
 """
 
 # Author = Abhinav
-# Date = 1 Output 2021 
+# Date = 1 Output 2021
 # Pourpose = Just for practise and imporving skills
 # Source =  [HackerRank](https://www.hackerrank.com/challenges/cavity-map/problem)
 
 # Solution :
+
+# Complete the 'cavityMap' function below.
+
+# The function is expected to return a STRING_ARRAY.
+# The function accepts STRING_ARRAY grid as parameter.
+
+
+def cavityMap(grid: list) -> list:
+    grid = [list(_) for _ in grid]
+
+    for _ in range(1, n-1):
+        for i in range(1, n-1):
+            if grid[_][i] > grid[_-1][i] and grid[_][i] > grid[_+1][i] and grid[_][i] > grid[_][i-1] and grid[_][i] > grid[_][i+1]:
+                grid[_][i] = "X"
+    
+    return ["".join(_) for _ in grid]
+
+
+if __name__ == '__main__':
+    n = int(input())
+    grid = list()
+    for _ in range(n): grid.append(input())
+    result = cavityMap(grid)
+    print('\n'.join(result))
