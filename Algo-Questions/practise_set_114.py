349ac5fb084d6ce4bfc84750dad868d1efa769c0
Brodevil <brodevil89@gmail.com>
2021-10-02 19:38:40 +0530

    Question for tommorow

---
+++ Algo-Questions/practise_set_114.py
@@ -0,0 +1,18 @@
+# Algorithm Practise 114 = Manasa and Stones (From HackerRank)
+
+"""
+Manasa and Stones |
+
+
+Get the Problem Statement on HackerRank : https://www.hackerrank.com/challenges/manasa-and-stones/problem
+
+And get the Solved solution in python, here :|
+
+"""
+
+# Author = Abhinav
+# Date = 2 Output 2021 (Gandhi Ji)
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/manasa-and-stones/problem)
+
+# Solution :
