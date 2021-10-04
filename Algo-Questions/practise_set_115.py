b56ed27cb9fcd03383b74d1441aa125a72dfdedd
Brodevil <brodevil89@gmail.com>
2021-10-04 20:59:31 +0530

    Intial commit

--- Algo-Questions/practise_set_115.py
+++ Algo-Questions/practise_set_115.py
@@ -1,1 +1,18 @@
-# Algorithm Question 115  =  
+# Algorithm Question 115  =  A. Simply Strange Sort (From CodeForces)
+
+"""
+A. Simply Strange Sort |
+
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1561/A
+
+And get the solved solution in python, here :}
+
+"""
+
+# Author = Abhinav
+# Date = 4 Output 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeForces](https://codeforces.com/problemset/problem/1561/A)
+
+# Solution :
