de3fc88aa4007c5506b5e386a522b5e1c2af48f3
Brodevil <brodevil89@gmail.com>
2021-10-05 21:04:26 +0530

    Intial commit

--- Algo-Questions/practise_set_115.py
+++ Algo-Questions/practise_set_115.py
@@ -16,3 +16,17 @@ And get the solved solution in python, here :}
 # Source =  [CodeForces](https://codeforces.com/problemset/problem/1561/A)
 
 # Solution :
+
+for _ in range(int(input())):
+    n, a = int(input()), list(map(int, input().split()))
+    iterate = 0
+    while a != sorted(a):
+        condition = False
+        for _ in range(n):
+            if _ != n-1 and a[_] > a[_+1]:
+                a[_], a[_+1] = a[_+1], a[_]
+                condition = True
+        if condition:
+            iterate += 1
+            print(a, iterate)
+    print(iterate)
