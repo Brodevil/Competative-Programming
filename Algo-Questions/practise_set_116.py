d38636cc32d1d6d32a2a171659bd27b992674fe0
Brodevil <brodevil89@gmail.com>
2021-10-07 19:56:19 +0530

    Initial commit

---
+++ Algo-Questions/practise_set_116.py
@@ -0,0 +1,18 @@
+# Algorithm Question 116  =  Fair Rations (From HackerRank)
+
+"""
+Fair Rations |
+
+
+Get the Problem Statement on HackerRank : https://www.hackerrank.com/challenges/fair-rations/problem
+
+And get the solved solution in python, here :|
+
+"""
+
+# Author = Abhinav
+# Date = 7 Output 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeForces](https://codeforces.com/problemset/problem/1561/A)
+
+# Solution :
