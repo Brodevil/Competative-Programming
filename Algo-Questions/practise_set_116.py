9d167cd3e8db94cd4312f33f62ca9f0977b139b5
Brodevil <brodevil89@gmail.com>
2021-10-08 20:09:49 +0530

    basic input by HackerRank

--- Algo-Questions/practise_set_116.py
+++ Algo-Questions/practise_set_116.py
@@ -13,6 +13,24 @@ And get the solved solution in python, here :|
 # Author = Abhinav
 # Date = 7 Output 2021
 # Pourpose = Just for practise and imporving skills
-# Source =  [CodeForces](https://codeforces.com/problemset/problem/1561/A)
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/fair-rations/problem)
 
 # Solution :
+
+# Complete the 'fairRations' function below.
+
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY B as parameter.
+
+
+
+def fairRations(B: list) -> int:
+    
+    return 0
+
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    B = list(map(int, input().rstrip().split()))
+    result = fairRations(B)
+    print(result)
