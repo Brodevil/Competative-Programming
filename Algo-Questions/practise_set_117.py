d0576a76efe2eda10f34bae380f6c1feaa432738
Brodevil <brodevil89@gmail.com>
2021-10-11 20:37:08 +0530

    Solved the Question

--- Algo-Questions/practise_set_117.py
+++ Algo-Questions/practise_set_117.py
@@ -16,3 +16,15 @@ And get the solved solution in python, here :|
 # Source =  [HackerRank](https://www.hackerrank.com/challenges/pangrams/problem)
 
 # Solution :
+
+from string import ascii_lowercase
+
+if __name__ == "__main__":
+    n = input().lower()
+    for _ in ascii_lowercase:
+        if _ not in n:
+            print("not pangram")
+            break
+    else:
+        print("pangram")
+
