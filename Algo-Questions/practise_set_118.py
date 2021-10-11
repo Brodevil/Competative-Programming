987e4a1ea149c8dc525261df751840b4e5267e70
Brodevil <brodevil89@gmail.com>
2021-10-11 21:04:32 +0530

    Intial commit

---
+++ Algo-Questions/practise_set_118.py
@@ -0,0 +1,18 @@
+# Algorithm Question 118  =  Weighted Uniform Strings (From HackerRank)
+
+"""
+Weighted Uniform Strings |
+
+
+Get the Problem Statement on HackerRank : https://www.hackerrank.com/challenges/weighted-uniform-string/problem
+
+And get the solved solution in python, here :|
+
+"""
+
+# Author = Abhinav
+# Date = 11 Output 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/weighted-uniform-string/problem)
+
+# Solution :
