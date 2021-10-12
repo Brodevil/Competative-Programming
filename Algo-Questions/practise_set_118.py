be1f4b764465016ac0881416b60caf6cc0a8371f
Brodevil <brodevil89@gmail.com>
2021-10-12 20:58:28 +0530

    wanna going to comtinure this code tommorow

--- Algo-Questions/practise_set_118.py
+++ Algo-Questions/practise_set_118.py
@@ -16,3 +16,22 @@ And get the solved solution in python, here :|
 # Source =  [HackerRank](https://www.hackerrank.com/challenges/weighted-uniform-string/problem)
 
 # Solution :
+
+from string import ascii_lowercase as lc
+
+if __name__ == "__main__":
+    s = input()
+    queary = list()
+    for _ in range(int(input())):
+        queary.append(int(input()))
+    
+
+    for _ in s:
+        weight = lc.index(_)+1
+        for i in range(1, s.count(_)+1):
+            print(_, i)
+            if weight*i in queary:
+                print("YES")
+            else:
+                print("NO")
+        
