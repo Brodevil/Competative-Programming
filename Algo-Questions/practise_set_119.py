b97efcbaccaafedaf8a59f76312a63323337ab22
Brodevil <brodevil89@gmail.com>
2021-10-14 20:21:04 +0530

    Intial Commit

--- Algo-Questions/practise_set_119.py
+++ Algo-Questions/practise_set_119.py
@@ -1,1 +1,18 @@
-# Algorithm Question 119 = 
+# Algorithm Question 119 = Separate the Numbers (From HackerRank)
+
+"""
+Separate the Numbers |
+
+
+Get the Problem Statment on HackerRank : https://www.hackerrank.com/challenges/separate-the-numbers/problem
+
+And get the solved solution in python... here:|
+
+"""
+
+# Author = Abhinav
+# Date = 14 Output 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/separate-the-numbers/problem)
+
+# Solution :
