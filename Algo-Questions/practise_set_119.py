227e83316773609e5d6cadbe5ff0413951595c69
Brodevil <brodevil89@gmail.com>
2021-10-17 19:39:59 +0530

    Solved the Question

--- Algo-Questions/practise_set_119.py
+++ Algo-Questions/practise_set_119.py
@@ -17,11 +17,24 @@ And get the solved solution in python... here:|
 
 # Solution :
 
+best = None
 
 for _ in range(int(input())):
     s = input()
-    if len(s) == 1:
-        print("NO")
-    for _ in range(len(s)):
-        n = s[_]
+    found = False
+
+    for i in range(len(s)//2):
+        a = s[:i+1]
+        f = n = int(s[:i+1])
+
+        while len(a) < len(s):
+            n += 1
+            a += str(n)
         
+        if a == s:
+            found = True
+            print('YES', f)
+            break
+        
+    if not found:
+        print('NO')
