9891d9a5022167b7703a591f8248d6a703a379e1
Brodevil <brodevil89@gmail.com>
2021-10-17 21:06:59 +0530

    Intial commit

---
+++ Algo-Questions/practise_set_121.py
@@ -0,0 +1,18 @@
+# Algorithm Question 121 =  A. Consecutive Sum Riddle (From CodeForces)
+
+"""
+A. Consecutive Sum Riddle |
+
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1594/A
+
+And get the solved solution in python, here:|
+
+"""
+
+# Author = Abhinav
+# Date = 17 Output 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeFroces](https://codeforces.com/problemset/problem/1594/A)
+
+# Solution :
