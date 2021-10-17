c59b923da87cc14bb4122012835be7c300f4da29
Brodevil <brodevil89@gmail.com>
2021-10-17 19:43:53 +0530

    Intial Commit

---
+++ Algo-Questions/practise_set_120.py
@@ -0,0 +1,19 @@
+# Algorithm Question 120  =  A. Computer Game (From CodeForces)
+
+"""
+A. Computer Game |
+
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1598/A
+
+And get the solved solution in python, here :|
+
+"""
+
+# Author = Abhinav
+# Date = 17 Output 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeFroces](https://codeforces.com/problemset/problem/1598/A)
+
+# Solution :
+
