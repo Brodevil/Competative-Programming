5d4bdced69530bd293ab8804d06d942325072387
Brodevil <brodevil89@gmail.com>
2021-10-18 19:43:31 +0530

    file template for solving another problem

---
+++ Algo-Questions/practise_set_122.py
@@ -0,0 +1,18 @@
+# Algorithm Question 122 = A. Elections (From CodeForces)
+
+"""
+A. Elections |
+
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1593/A
+
+And get the solved solution in python, here :}
+
+"""
+
+# Author = Abhinav
+# Date = 18 Output 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeFroces](https://codeforces.com/problemset/problem/1593/A)
+
+# Solution :
