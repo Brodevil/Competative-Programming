fe5d134a1b87ab27ae25e95afbc688fbb76d7b10
Brodevil <brodevil89@gmail.com>
2021-10-19 20:02:47 +0530

    Intial Commit

---
+++ Algo-Questions/practise_set_123.py
@@ -0,0 +1,18 @@
+# Algorithm Question 123  =  A. Windblume Ode (From CodeForces)
+
+"""
+A. Windblume Ode |
+
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1583/A
+
+And get the solved solution in python, here :}
+
+"""
+
+# Author = Abhinav
+# Date = 19 Output 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeFroces](https://codeforces.com/problemset/problem/1583/A)
+
+# Solution :
