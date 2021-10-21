36a5b29d0a86546f1dec3666bfbf206e039a29b9
Brodevil <brodevil89@gmail.com>
2021-10-21 19:01:27 +0530

    Really it's a hard question:

--- Algo-Questions/practise_set_123.py
+++ Algo-Questions/practise_set_123.py
@@ -20,4 +20,19 @@ And get the solved solution in python, here :}
 for _ in range(int(input())):
     n = int(input())
     arr = list(map(int, input().split()))
+    d = sum(a)
+    condition = True
     
+    for i in range(2, round(d ** 0.5) + 1):
+        if d % i == 0:
+            condition = False
+            break
+    if not condition:
+        print(n)
+        print(*[i + 1 for i in range(n)])
+    else:
+        for i in range(n):
+            if a[i] % 2 == 1:
+                print(n - 1)
+                print(*[j + 1 for j in range(n) if i != j])
+                break
