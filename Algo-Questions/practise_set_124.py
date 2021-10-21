24a8e5c24c11c2d4f94b28130f5492de42567e1e
Brodevil <brodevil89@gmail.com>
2021-10-21 19:18:13 +0530

    Intial Commit

--- Algo-Questions/practise_set_124.py
+++ Algo-Questions/practise_set_124.py
@@ -1,1 +1,18 @@
-# Algorithm Question 124  =  
+# Algorithm Question 124  =  Happy Ladybugs (From HackeRRank)
+
+"""
+Happy Ladybugs |
+
+
+Get the Problem Statement on HackerRank : https://www.hackerrank.com/challenges/happy-ladybugs/problem
+
+And get the solved solution in python, here :}
+
+"""
+
+# Author = Abhinav
+# Date = 21 Output 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank]](https://www.hackerrank.com/challenges/happy-ladybugs/problem)
+
+# Solution :
