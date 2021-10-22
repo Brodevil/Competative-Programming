76ff48f5b117e71b79e0f5da72e4eaa8b19460dc
Brodevil <brodevil89@gmail.com>
2021-10-22 19:27:04 +0530

    Intial Commit

---
+++ Algo-Questions/practise_set_125.py
@@ -0,0 +1,18 @@
+# Algorithm Question 125  =  Strange Counter (From HackerRank)
+
+"""
+Strange Counter |
+
+
+Get the Problem Statment on HackerRank : https://www.hackerrank.com/challenges/strange-code/problem
+
+And get the solved solution in python, here :}
+
+"""
+
+# Author = Abhinav
+# Date = 22 Output 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/strange-code/problem)
+
+# Solution :
