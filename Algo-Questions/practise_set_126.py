f5e7d0872e1f836e910c47b70e998b9492a1b954
Brodevil <brodevil89@gmail.com>
2021-10-23 20:00:30 +0530

    Intial Commit

---
+++ Algo-Questions/practise_set_126.py
@@ -0,0 +1,18 @@
+# Algorithm Practise 126  =  A. Regular Bracket Sequences (From CodeForces)
+
+"""
+A. Regular Bracket Sequences |
+
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1574/A
+
+And get the Solution, solved by me, here :}
+
+"""
+
+# Author = Abhinav
+# Date = 23 Output 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeForces](https://codeforces.com/problemset/problem/1574/A)
+
+# Solution :
