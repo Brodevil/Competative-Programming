5e5230a762e7ff89b99dc3d1b5c4f7ec8edc3f9b
Brodevil <brodevil89@gmail.com>
2021-10-25 19:21:54 +0530

    Intial Commit

---
+++ Algo-Questions/practise_set_128.py
@@ -0,0 +1,18 @@
+# Algorithm Question 128  =  A. Gamer Hemose (From CodeForces)
+
+"""
+A. Gamer Hemose |
+
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1592/A
+
+And get the solution, solved by me, here :}
+
+"""
+
+# Author = Abhinav
+# Date = 25 Output 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeFroces](https://codeforces.com/problemset/problem/1592/A)
+
+# Solution :
