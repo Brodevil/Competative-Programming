977714d8c40d31d2f398cd89c8e529be4cba5692
Brodevil <brodevil89@gmail.com>
2021-10-27 21:13:58 +0530

    Solved the Question, used the leetcode for first time

---
+++ Algo-Questions/practise_set_130.py
@@ -0,0 +1,26 @@
+# Algorithm Question 130  =  Two Sum (From LeetCode)
+
+"""
+Two Sum |
+
+
+Get the Problem Statement on LeetCode : https://leetcode.com/problems/two-sum/submissions/
+
+And get the Solved solution by me, in python, here :}
+
+"""
+
+# Author = Abhinav
+# Date = 27 Output 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [LeetCode](https://leetcode.com/problems/two-sum/submissions/)
+
+# Solution :
+
+nums = list(map(int, input().split()))
+target = int(input())
+
+for i in range(len(nums)):
+    for _ in range(len(nums)):
+        if i != _ and (nums[i] + nums[_]) == target:
+            print([i, _])
