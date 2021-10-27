459e34d91e6154fe8d98ccb57067254a9aa6157a
Brodevil <brodevil89@gmail.com>
2021-10-27 20:34:18 +0530

    Stander input

--- Algo-Questions/practise_set_129.py
+++ Algo-Questions/practise_set_129.py
@@ -16,3 +16,7 @@ And get the solution, solved by me, here :}
 # Source =  [HackerRank](https://www.hackerrank.com/challenges/richie-rich/problem)
 
 # Solution :
+
+if __name__ == "__main__":
+    n, k = map(int, input().split())
+    s = input()
