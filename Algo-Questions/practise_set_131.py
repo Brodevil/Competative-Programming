f71ba8fe5db4c0622c92bdb2fef6f4b39f809834
Brodevil <brodevil89@gmail.com>
2021-10-28 18:37:35 +0530

    Basic Solution by LeetCode

--- Algo-Questions/practise_set_131.py
+++ Algo-Questions/practise_set_131.py
@@ -16,3 +16,14 @@ And get the solution, solved by me, here :}
 # Source =  [LeetCode](https://leetcode.com/problems/two-sum/submissions/)
 
 # Solution :
+
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        pass
+        
