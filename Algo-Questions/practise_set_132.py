4e1f765bf9b052798a3ef9913e91f41d278fc899
Brodevil <brodevil89@gmail.com>
2021-10-29 19:56:39 +0530

    In Process to solve

--- Algo-Questions/practise_set_132.py
+++ Algo-Questions/practise_set_132.py
@@ -16,3 +16,38 @@ And get the solution in python here :}, solved by me.
 # Source =  [The Gride Search](https://www.hackerrank.com/challenges/the-grid-search/problem)
 
 # Solution :
+
+
+for _ in range(int(input())):
+    R, C = map(int, input().split())
+    G = list()
+    for _ in range(R):
+        G.append(input())
+    
+    r, c = map(int, input().split())
+    P = list()
+    for _ in range(r):
+        P.append(input())
+    
+    index = 0
+    match = False
+    iteration = 1
+    
+    for _ in G:
+        if not match:
+            if P[0] in _:
+                print(_, P[0])
+                match = True
+                index = _.index(P[0])
+        else:
+            print(_, P[iteration], _[index:c-1])
+            if iteration == c:
+                print("YES")
+                break
+
+            elif _[index:c-1] != P[iteration]:
+                match = False
+            else:
+                iteration += 1
+    else:
+        print("NO")
