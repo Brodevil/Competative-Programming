15d97b1b8a53d33e7a31e6fee628fbaffa5daa9e
Brodevil <brodevil89@gmail.com>
2021-10-30 19:42:03 +0530

    Intail commit

---
+++ Algo-Questions/practise_set_133.py
@@ -0,0 +1,18 @@
+# Algorithm Question 133  =  A. Array Elimination  (Form CodeFroces)
+
+"""
+A. Array Elimination |
+
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1601/A
+
+And get the solution, solved by me, here :}
+
+"""
+
+# Author = Abhinav
+# Date = 30 Output 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeFroces](https://codeforces.com/problemset/problem/1601/A)
+
+# Solution :
