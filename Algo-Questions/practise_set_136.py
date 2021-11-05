899445b67b80dd4adc5c56a86990ccb674238f24
Brodevil <brodevil89@gmail.com>
2021-11-05 20:14:27 +0530

    Intial Commit

---
+++ Algo-Questions/practise_set_136.py
@@ -0,0 +1,18 @@
+# Algorithm Question 136  =  Insertion Sort - Part 1 (From HackerRank)
+
+"""
+Insertion Sort - Part 1 |
+
+
+Get the Problem statement on HackerRank : https://www.hackerrank.com/challenges/insertionsort1/problem
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 6 November 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/insertionsort1/problem)
+
+# Solution :
