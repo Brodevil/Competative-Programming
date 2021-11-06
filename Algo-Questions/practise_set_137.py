c62a70e37456a61ab89c17372db9c3aede72feb6
Brodevil <brodevil89@gmail.com>
2021-11-06 19:20:23 +0530

    Intial Commit

---
+++ Algo-Questions/practise_set_137.py
@@ -0,0 +1,18 @@
+# Compitative Programming Question 137  =  Insertion Sort - Part 2  (From HackerRank)
+
+"""
+Insertion Sort - Part 2 |
+
+
+Get the problem statement on HackerRank : https://www.hackerrank.com/challenges/insertionsort2/problem
+
+And get solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 7 November 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/insertionsort2/problem)
+
+# Solution :
