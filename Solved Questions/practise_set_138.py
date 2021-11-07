09bcb4009cc102321de7480d205925e8f300b573
Brodevil <brodevil89@gmail.com>
2021-11-07 21:01:45 +0530

    Intial Commit

---
+++ Solved Questions/practise_set_138.py
@@ -0,0 +1,18 @@
+# Algorithm Question 138  =  Intro to Tutorial Challenges (From HackerRank)
+
+"""
+Intro to Tutorial Challenges |
+
+
+Get the Problem Satement on HackerRank : https://www.hackerrank.com/challenges/tutorial-intro/problem
+
+And get the solution here, solved by me in python :}
+
+"""
+
+# Author = Abhinav
+# Date = 8 November 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/insertionsort2/problem)
+
+# Solution :
