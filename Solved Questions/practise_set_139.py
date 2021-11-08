0731f625559e70c84a3e95f3a87157b1733e2b7b
Brodevil <brodevil89@gmail.com>
2021-11-08 19:40:32 +0530

    Initial Commit

---
+++ Solved Questions/practise_set_139.py
@@ -0,0 +1,18 @@
+# Competitive Programming Question 139  =  Correctness and the Loop Invariant (From HackerRank)
+
+"""
+Correctness and the Loop Invariant |
+
+
+Get the problem statement on HackerRank : https://www.hackerrank.com/challenges/correctness-invariant/problem
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 9 November 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/correctness-invariant/problem)
+
+# Solution :
