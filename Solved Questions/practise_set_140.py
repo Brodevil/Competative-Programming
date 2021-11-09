22f422e1257a73ae9e0255a682d4656783f3042b
Brodevil <brodevil89@gmail.com>
2021-11-09 19:29:52 +0530

    Intial Commit

---
+++ Solved Questions/practise_set_140.py
@@ -0,0 +1,18 @@
+# Competitive Programming Question 140  =  Word Order (From HackerRank)
+
+"""
+Word Order |
+
+
+Get the Problem Statement on HackerRnak : https://www.hackerrank.com/challenges/word-order/problem
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 10 November 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/word-order/problem)
+
+# Solution :
