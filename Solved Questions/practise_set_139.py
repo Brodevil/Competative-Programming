c80feb115ea73b3690a11aa6e4a1eac435c430e9
Brodevil <brodevil89@gmail.com>
2021-11-09 19:23:37 +0530

    Not Getting to do this in python

--- Solved Questions/practise_set_139.py
+++ Solved Questions/practise_set_139.py
@@ -16,3 +16,13 @@ And get the solution here, solved in python by me :}
 # Source =  [HackerRank](https://www.hackerrank.com/challenges/correctness-invariant/problem)
 
 # Solution :
+
+if __name__ == "__main__":
+    s = int(input())
+    arr = list(map(int, input().split()))
+    for i in range(s):
+        while i != 0 and arr[i] < arr[i-1]:
+            arr[i], arr[i-1] = arr[i-1], arr[i]
+            i-=1
+
+    print(*arr)
