81ff9ed0aa1310015aa38d3d6a87b9ec49d3f567
Brodevil <brodevil89@gmail.com>
2021-11-12 19:27:09 +0530

    Intial commit

---
+++ Solved Questions/practise_set_141.py
@@ -0,0 +1,18 @@
+# Question 141  =  HackerRank in a String! (From HackerRank)
+
+"""
+HackerRank in a String! |
+
+
+Get the Problem Statement on HackerRank :  https://www.hackerrank.com/challenges/hackerrank-in-a-string/problem
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 12 November 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/correctness-invariant/problem)
+
+# Solution :
