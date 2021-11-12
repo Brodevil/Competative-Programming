aca885a6faed0d2f7633a8057af8c90ad64536f1
Brodevil <brodevil89@gmail.com>
2021-11-12 19:58:25 +0530

    Intial Commit

---
+++ Solved Questions/practise_set_142.py
@@ -0,0 +1,18 @@
+# Copetative Programming Question 142  =  A. Linear Keyboard  (From CodeForces)
+
+"""
+A. Linear Keyboard |
+
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1607/A
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 12 November 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeFroces](https://codeforces.com/problemset/problem/1607/A)
+
+# Solution :
