74c0e10766b0613c440f9c824b16ce0d997db9bd
Brodevil <brodevil89@gmail.com>
2021-11-17 20:36:05 +0530

    Intial Commit

---
+++ Solved Questions/practise_set_143.py
@@ -0,0 +1,18 @@
+# Competitive Programming Question 143  =  B. Odd Grasshopper (From CodeForces)
+
+"""
+B. Odd Grasshopper |
+
+
+Get the problem Statement on CodeForces : https://codeforces.com/problemset/problem/1607/B
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 17 November 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeFroces](https://codeforces.com/problemset/problem/1607/B)
+
+# Solution :
