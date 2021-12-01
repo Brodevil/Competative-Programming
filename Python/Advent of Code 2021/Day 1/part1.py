c8ee34d72bcfe201df70e624aa35960e2a6cc88e
Brodevil <brodevil89@gmail.com>
2021-12-01 19:44:30 +0530

    rewriting the solution

---
+++ Python/Advent of Code 2021/Day 1/part1.py
@@ -0,0 +1,17 @@
+# Advent of code 2021 : Day 1 | Part 1
+
+# Author = Abhinav
+# Date = 1st of December 2021
+# Source =  [Advent Of Code](https://adventofcode.com/2021/day/1)
+
+# Solution : 
+
+
+if __name__ == "__main__":
+    inputs = open("input.txt", "rt")
+    inputs.readlines()
+
+    for _ in inputs:
+        print(_)
+
+        
