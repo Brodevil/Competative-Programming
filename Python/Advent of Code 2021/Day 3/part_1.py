717760ade1ca1712c74a8655f72a1ac15a5e149b
Brodevil <brodevil89@gmail.com>
2021-12-03 11:01:13 +0530

    got 1 star

--- Python/Advent of Code 2021/Day 3/part_1.py
+++ Python/Advent of Code 2021/Day 3/part_1.py
@@ -6,6 +6,22 @@
 
 # Solution : 
 
+inputs = open("input.txt", "rt")
+inputs = inputs.read().splitlines()
+
+gamma, epsilon = "", ""
+
+for i in range(len(inputs[0])): 
+    s = ''.join([_[i] for _ in inputs])
+    _0, _1 = s.count("1"), s.count("0")
+    if _0 > _1:
+        gamma += "0"
+        epsilon += "1"
+    elif _1 > _0:
+        gamma += "1"
+        epsilon += '0'
+
+print(int(gamma, 2) * int(epsilon, 2))
 
 
-# Answer : 
+# Answer : 1071734
