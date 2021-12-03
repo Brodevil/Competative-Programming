58e842f8fbf1392cb2772571ce5abefd659d9ff2
Brodevil <brodevil89@gmail.com>
2021-12-03 19:25:04 +0530

    Intial Commit

--- Python/Advent of Code 2021/Day 3/part_1.py
+++ Python/Advent of Code 2021/Day 3/part_1.py
@@ -1,7 +1,7 @@
 # Advent of code 2021 : Day 3 | Part 1
 
 # Author = Abhinav
-# Date = 1st of December 2021
+# Date = 3rd of December 2021
 # Source =  [Advent Of Code](https://adventofcode.com/2021/day/3)
 
 # Solution : 
---
+++ Python/Advent of Code 2021/Day 4/part_1.py
@@ -0,0 +1,11 @@
+# Advent of code 2021 : Day 4 | Part 1
+
+# Author = Abhinav
+# Date = 4th of December 2021
+# Source =  [Advent Of Code](https://adventofcode.com/2021/day/4)
+
+# Solution : 
+
+
+
+# Answer : 
