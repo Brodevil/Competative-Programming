26851162caea5484d834fe0d0c0543c04ce17d30
Brodevil <brodevil89@gmail.com>
2021-12-03 19:16:44 +0530

    Abhinav Choudhary 6*

--- Python/Advent of Code 2021/Day 3/part_2.py
+++ Python/Advent of Code 2021/Day 3/part_2.py
@@ -2,10 +2,35 @@
 
 # Author = Abhinav
 # Date = 3rd of December 2021
-# Source =  [Advent Of Code](https://adventofcode.com/2021/day/3)
+# Source =  [Advent Of Code](https://adventofcode.com/2021/day/3#part2)
 
-# Solution : 
+# Solution :
 
 
+inputs = open("input.txt", "r")
+inputs = inputs.read().splitlines()
 
-# Answer : 
+
+A = list(inputs)
+B = list(inputs)
+
+for i in range(len(inputs[0])):
+    if len(A) > 1:
+        a0 = len([x for x in A if x[i] == '0'])
+        a1 = len([x for x in A if x[i] == '1'])
+        if a1 >= a0:
+            A = [x for x in A if x[i] == '1']
+        else:
+            A = [x for x in A if x[i] == '0']
+    
+    if len(B) > 1:
+        b0 = len([x for x in B if x[i] == '0'])
+        b1 = len([x for x in B if x[i] == '1'])
+        if b1 >= b0:
+            B = [x for x in B if x[i] == '0']
+        else:
+            B = [x for x in B if x[i] == '1']
+
+print(int(A[0], 2) * int(B[0], 2))
+
+# Answer : 6124992
