f090149c886067a2b67444fb81223d2ac89b2a32
Brodevil <brodevil89@gmail.com>
2021-12-11 19:04:32 +0530

    Intial Commit

---
+++ Python/Solved Questions/practise_set_144.py
@@ -0,0 +1,18 @@
+# Copetative Programming Question 144  = A. Divan and a Store  (From CodeForces)
+
+"""
+A. Divan and a Store |
+
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1614/A
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 12 December 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeFroces](https://codeforces.com/problemset/problem/1614/A)
+
+# Solution :
