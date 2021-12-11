a3529b1f2f6d3d191f32c66f61c7c2d5a0da3037
Brodevil <brodevil89@gmail.com>
2021-12-11 20:00:04 +0530

    Intial Commit

---
+++ Python/Solved Questions/practise_set_145.py
@@ -0,0 +1,18 @@
+# Copetative Programming Question 145  = B. Divan and a New Project  (From CodeForces)
+
+"""
+B. Divan and a New Project |
+
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1614/B
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 12 December 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeFroces](https://codeforces.com/problemset/problem/1614/B)
+
+# Solution :
