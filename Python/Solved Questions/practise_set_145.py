b89efa17e86138d85a67dd3a7f9988c31ce79821
Brodevil <brodevil89@gmail.com>
2021-12-12 19:12:52 +0530

    Solved the Question

--- Python/Solved Questions/practise_set_145.py
+++ Python/Solved Questions/practise_set_145.py
@@ -16,3 +16,23 @@ And get the solution here, solved in python by me :}
 # Source =  [CodeFroces](https://codeforces.com/problemset/problem/1614/B)
 
 # Solution :
+
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    res = [0] * (n + 1)
+    s = 0
+    b = sorted([[a[i], i] for i in range(len(a))], reverse=True)
+    pos = 1
+    
+    for k, i in b:
+        res[i + 1] = pos
+        s += 2 * k * abs(pos)
+        if pos > 0:
+            pos = -pos
+        else:
+            pos = -pos + 1
+    
+    print(s)
+    print(*res)
