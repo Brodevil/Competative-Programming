45cafe2e5c151db379947718353beb534283068a
Brodevil <brodevil89@gmail.com>
2021-12-15 20:14:55 +0530

    Initial Commit

---
+++ Python/Solved Questions/practise_set_148.py
@@ -0,0 +1,17 @@
+# Copetative Programming Question 147  = Quicksort 1 - Partition  (From HackerRank)
+
+"""
+Quicksort 1 - Partition |
+
+Get the Problem Statement on CodeForces : https://www.hackerrank.com/challenges/quicksort1/problem
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 15 December 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/quicksort1/problem)
+
+# Solution :
