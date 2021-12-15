fb865a4671ac96dc708fbec6c7cb670487922330
Brodevil <brodevil89@gmail.com>
2021-12-15 20:33:53 +0530

    Initial Commit

---
+++ Python/Solved Questions/practise_set_149.py
@@ -0,0 +1,17 @@
+# Copetative Programming Question 147  = Quicksort 2 - Sorting  (From HackerRank)
+
+"""
+Quicksort 2 - Sorting |
+
+Get the Problem Statement on CodeForces : https://www.hackerrank.com/challenges/quicksort2/problem
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 16 December 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/quicksort2/problem)
+
+# Solution :
