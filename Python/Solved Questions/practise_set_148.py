a1ac878b5adc219f5d07be7abdc3b8da44b5233e
Brodevil <brodevil89@gmail.com>
2021-12-15 20:31:35 +0530

    Solved the question

--- Python/Solved Questions/practise_set_148.py
+++ Python/Solved Questions/practise_set_148.py
@@ -15,3 +15,18 @@ And get the solution here, solved in python by me :}
 # Source =  [HackerRank](https://www.hackerrank.com/challenges/quicksort1/problem)
 
 # Solution :
+
+n = int(input())
+arr = list(map(int, input().split()))
+p = arr[0]
+right = list()
+left = list()
+
+for _ in range(1, n):
+    _ = arr[_]
+    if _ > p:
+        right.append(_)
+    elif _ < p:
+        left.append(_)
+
+print(*[*left, p, *right])
