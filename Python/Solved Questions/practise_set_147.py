e562156a9efecc41eacd13740fd502917efccd58
Brodevil <brodevil89@gmail.com>
2021-12-15 19:02:19 +0530

    Initial Commit

---
+++ Python/Solved Questions/practise_set_147.py
@@ -0,0 +1,17 @@
+# Copetative Programming Question 147  = Running Time of Algorithm  (From HackerRank)
+
+"""
+Running Time of Algorithms |
+
+Get the Problem Statement on CodeForces : https://www.hackerrank.com/challenges/runningtime/problem
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 15 December 2021
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/runningtime/problem)
+
+# Solution :
