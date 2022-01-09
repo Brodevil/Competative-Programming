50bb36e489c36b54fb1b44eccf425a2899f0cd5e
Brodevil <brodevil89@gmail.com>
2022-01-09 20:59:21 +0530

    Initial Commit

--- Python/Solved Questions/practise_set_150.py
+++ Python/Solved Questions/practise_set_150.py
@@ -15,3 +15,32 @@ And get the solution here, solved in python by me :}
 # Source =  [HackerRank](https://www.hackerrank.com/challenges/two-characters/problem)
 
 # Solution :
+
+def validate(inp):
+    for i in range(len(inp)-1):
+        if inp[i+1]==inp[i]:
+            return False
+    return True    
+    
+
+s_len = int(input())
+s = input()
+
+ans = 0
+chtoindex = list()
+for ch in set(s):
+    chtoindex.append((ch, len([j for j, x in enumerate(s) if x == ch])))
+    
+
+for i, pack in enumerate(chtoindex[:-1]):
+    char_i, lenchar_i = pack[0], pack[1]
+    
+    for j, otherpack in enumerate(chtoindex[i+1:]):
+        char_j, lenchar_j = otherpack[0], otherpack[1]
+        if abs(lenchar_i-lenchar_j)<2:
+            c = [cha for cha in s if cha is char_i or cha is char_j]
+            
+            if validate(c):
+                ans = max(ans, lenchar_j+lenchar_i)
+
+print(ans)
