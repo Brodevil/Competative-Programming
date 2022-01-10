582d13f7cbc2b5b40dceeee8a570807a78631cd7
Brodevil <brodevil89@gmail.com>
2022-01-10 19:23:13 +0530

    Initial commit

---
+++ Python/Solved Questions/practise_set_152.py
@@ -0,0 +1,17 @@
+# Competative Programming Question 152 = Mars Exploration (From HackerRank)
+
+"""
+Mars Exploration |
+
+Get the Problem Statement on HackerRank : https://www.hackerrank.com/challenges/mars-exploration/problem
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 11 January 2022
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/mars-exploration/problem)
+
+# Solution :
