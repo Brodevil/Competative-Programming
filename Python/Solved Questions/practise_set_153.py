ca5f95c2d73f46946062e9115025125d21122f40
Brodevil <brodevil89@gmail.com>
2022-01-14 19:33:42 +0530

    Initial Commit

---
+++ Python/Solved Questions/practise_set_153.py
@@ -0,0 +1,17 @@
+# Competative Programming Question 153 = A. Plus One on the Subset (From CodeForces )
+
+"""
+A. Plus One on the Subset |
+
+Get the Problem Statement on CodeFroces : https://codeforces.com/problemset/problem/1624/A
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 14 January 2022
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://codeforces.com/problemset/problem/1624/A)
+
+# Solution :
