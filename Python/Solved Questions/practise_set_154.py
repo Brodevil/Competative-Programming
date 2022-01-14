ff66d37d2ecb8a4b6013932621ce43c634950208
Brodevil <brodevil89@gmail.com>
2022-01-14 20:32:20 +0530

    Initial Commit

---
+++ Python/Solved Questions/practise_set_154.py
@@ -0,0 +1,17 @@
+# Competative Programming Question 154 = A. Construct a Rectangle (From CodeForces)
+
+"""
+A. Construct a Rectangle |
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1622/A
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 11 January 2022
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://codeforces.com/problemset/problem/1622/A)
+
+# Solution :
