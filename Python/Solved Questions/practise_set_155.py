4cc7930936c9bff784fe245dccb956a12d168ef2
Brodevil <brodevil89@gmail.com>
2022-01-16 20:51:46 +0530

    Initial Commit

---
+++ Python/Solved Questions/practise_set_155.py
@@ -0,0 +1,17 @@
+# Competative Programming Question 155 = Sequence Master (From CodeChef)
+
+"""
+Sequence Master |
+
+Get the Problem Statement on Sequence Master : https://www.codechef.com/problems/MASTER
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 16 January 2022
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeChef](https://www.codechef.com/problems/MASTER)
+
+# Solution :
