4696f55ef2d60e9a9c268089bfdeeab314ff67d7
Brodevil <brodevil89@gmail.com>
2022-01-20 19:55:28 +0530

    Initial Commit

---
+++ Python/Solved Questions/practise_set_158.py
@@ -0,0 +1,17 @@
+# Competative Programming Question 158 = Counting Sort 3 (From HackerRank)
+
+"""
+Counting Sort 3 |
+
+Get the Problem Statement on HackerRank : https://www.hackerrank.com/challenges/countingsort3/problem
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 20 January 2022
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/countingsort3/problem)
+
+# Solution :
