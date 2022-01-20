e6e5edd8ca301ba209ba81f7837fcc07c9d0c48b
Brodevil <brodevil89@gmail.com>
2022-01-20 19:54:19 +0530

    Solved it Bro

--- Python/Solved Questions/practise_set_157.py
+++ Python/Solved Questions/practise_set_157.py
@@ -15,3 +15,19 @@ And get the solution here, solved in python by me :}
 # Source =  [HackerRank](https://www.hackerrank.com/challenges/countingsort2/problem)
 
 # Solution :
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Sorted list in index form
+index_ls = [0]*100
+for _ in arr:
+    index_ls[_] += 1
+
+# Converting into sorted form
+ls = list()
+for i, j in enumerate(index_ls):
+    for _ in range(j):
+        ls.append(i)
+
+print(*ls)
