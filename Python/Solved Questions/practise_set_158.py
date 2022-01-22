6204a1b94c8778897f0f68be5f76bb2ef55b60f9
Brodevil <brodevil89@gmail.com>
2022-01-22 20:38:22 +0530

    Kabhi Kabhi lagta hai apunich bhagwan hai

--- Python/Solved Questions/practise_set_158.py
+++ Python/Solved Questions/practise_set_158.py
@@ -18,6 +18,15 @@ And get the solution here, solved in python by me :}
 
 arr = list()
 for _ in range(int(input())):
-    x, s = input().split()
-    arr.append([int(x), s])
+    x = int(input().split()[0])
+    arr.append(x)
 
+fq = [0]*100
+for _ in arr:
+    fq[_] += 1
+
+ans = list()
+for _ in range(1, 101):
+    ans.append(sum(fq[:_]))
+
+print(*ans)
