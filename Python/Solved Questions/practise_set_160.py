a3d137fc218ea7a62dacb1b60188468530d32861
Brodevil <brodevil89@gmail.com>
2022-01-23 19:38:36 +0530

    Initial Commit

---
+++ Python/Solved Questions/practise_set_160.py
@@ -0,0 +1,17 @@
+# Competative Programming Question 159 = Closest Numbers (From HackerRank)
+
+"""
+Closest Numbers |
+
+Get the Problem Statement on HackerRank : https://www.hackerrank.com/challenges/closest-numbers/problem
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 24 January 2022
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/closest-numbers/problem)
+
+# Solution :
