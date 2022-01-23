dc954b2c6e870b66c0c60e8a383561549d8ec94c
Brodevil <brodevil89@gmail.com>
2022-01-23 19:37:07 +0530

    Initial commit

--- Python/Solved Questions/practise_set_159.py
+++ Python/Solved Questions/practise_set_159.py
@@ -15,3 +15,21 @@ And get the solution here, solved in python by me :}
 # Source =  [HackerRank](https://www.hackerrank.com/challenges/countingsort4/problem)
 
 # Solution :
+
+n = int(input())
+arr = [[] for _ in range(n//2+1)]
+
+for _ in range(n):
+    x, s = input().split()
+    x = int(x)
+    if _ < n//2:
+        arr[x].append("-")
+    else:
+        arr[x].append(s)
+
+ans = ""
+for _ in arr:
+    for _0 in _:
+        ans += f"{_0} "
+
+print(ans)
