191a5130ceab7c49bb5b68c878a375613f1e1e5c
Brodevil <brodevil89@gmail.com>
2022-01-24 20:02:34 +0530

    I Solved it yes

--- Python/Solved Questions/practise_set_160.py
+++ Python/Solved Questions/practise_set_160.py
@@ -15,3 +15,19 @@ And get the solution here, solved in python by me :}
 # Source =  [HackerRank](https://www.hackerrank.com/challenges/closest-numbers/problem)
 
 # Solution :
+
+n = int(input())
+arr = sorted(list(map(int, input().split())))
+diff = abs(arr[0]-arr[1])
+ans = list()
+
+for _ in range(n-1):
+    d = abs(arr[_] - arr[_+1])
+    if diff == d:
+        ans += [arr[_], arr[_+1]]
+        diff = d
+    elif d < diff:
+        ans = [arr[_], arr[_+1]]
+        diff = d
+
+print(*ans)
