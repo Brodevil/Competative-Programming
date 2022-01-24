aea7d4bb14e5f8b590a0f778b866bfdb7e6853dd
Brodevil <brodevil89@gmail.com>
2022-01-24 20:07:32 +0530

    Initial commit

---
+++ Python/Solved Questions/practise_set_162.py
@@ -0,0 +1,17 @@
+# Competative Programming Question 162 = A. Ancient Civilization (From CodeForces)
+
+"""
+A. Ancient Civilization |
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1625/A
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 24 January 2022
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeForces](https://codeforces.com/problemset/problem/1625/A)
+
+# Solution :
