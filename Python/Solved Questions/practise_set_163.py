bb6292510e3f811506dff618d6e17e5ea439c9b5
Brodevil <brodevil89@gmail.com>
2022-01-30 20:26:16 +0530

    Medthod: BINARY Index was used and this is not my own solution

--- Python/Solved Questions/practise_set_163.py
+++ Python/Solved Questions/practise_set_163.py
@@ -16,16 +16,53 @@ And get the solution here, solved in python by me :}
 
 # Solution :
 
+
+def merge(arr, L, R):
+    i = il = ir = inversion_count = 0
+    l_len = len(L)
+    r_len = len(R)
+    
+    while il < l_len and ir < r_len:
+        if L[il] <= R[ir]:
+            arr[i] = L[il]
+            il += 1
+        else:
+            arr[i] = R[ir]
+            ir += 1
+            inversion_count += l_len - il
+        i += 1
+    
+    while il < l_len:
+        arr[i] = L[il]
+        il += 1
+        i += 1
+    
+    while ir < r_len:
+        arr[i] = R[ir]
+        ir += 1
+        i += 1
+
+    return inversion_count
+
+
+def merge_sort(arr):
+    mid = len(arr) // 2
+    inversion_count = 0
+    if mid > 0:
+        L = arr[:mid]
+        R = arr[mid:]
+        inversion_count += merge_sort(L)
+        inversion_count += merge_sort(R)
+        inversion_count += merge(arr, L, R)
+    return inversion_count
+
+
+def inversions(arr):
+    return merge_sort(arr)
+
+
 for _ in range(int(input())):
-    n = int(input())
+    n = input()
     arr = list(map(int, input().split()))
-    ans = 0
-
-    for _ in range(n - 1):
-        while arr[_] > arr[_ + 1]:
-            arr[_], arr[_ + 1] = arr[_ + 1], arr[_]
-            ans += 1
-            if _ != 0:
-                _ -= 1
-
-    print(ans)
+    counts = inversions(arr)
+    print(counts)
