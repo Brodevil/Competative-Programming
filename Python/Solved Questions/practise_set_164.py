fb1ab51545ccb1a50ff224e466db0056b021ee74
Brodevil <brodevil89@gmail.com>
2022-01-31 20:53:48 +0530

    Initial Commit

--- Python/Solved Questions/practise_set_164.py
+++ Python/Solved Questions/practise_set_164.py
@@ -15,3 +15,15 @@ And get the solution here, solved in python by me :}
 # Source =  [CodeForces](https://codeforces.com/problemset/problem/1631/A)
 
 # Solution :
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    
+    a_copy, b_copy = a.copy(), b.copy()
+    
+    for _ in range(n):
+        a_max, b_max = max(a_copy), max(b_copy)
+        
+        
