6dd8926e34721daa9af6a510737e513e4eae6aab
Brodevil <brodevil89@gmail.com>
2022-02-01 20:10:03 +0530

    Inital commit

---
+++ Python/Solved Questions/practise_set_165.py
@@ -0,0 +1,17 @@
+# Competative Programming Question 165 = B. Fun with Even Subarrays (From CodeForces)
+
+"""
+B. Fun with Even Subarrays |
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1631/B
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 1 February 2022
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeForces](https://codeforces.com/problemset/problem/1631/B)
+
+# Solution :
