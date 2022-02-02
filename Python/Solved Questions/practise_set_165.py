ce9b1cd1b07117a18a79fe443ec66df77acf353a
Brodevil <brodevil89@gmail.com>
2022-02-02 20:34:33 +0530

    It's a easy question

--- Python/Solved Questions/practise_set_165.py
+++ Python/Solved Questions/practise_set_165.py
@@ -15,3 +15,19 @@ And get the solution here, solved in python by me :}
 # Source =  [CodeForces](https://codeforces.com/problemset/problem/1631/B)
 
 # Solution :
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    ans = 0
+    a = a[::-1]
+    
+    _ = 1
+    while _ < n:
+        if a[0] != a[_]:
+            _ += _
+            ans += 1
+        else:
+            _ += 1
+    
+    print(ans)
