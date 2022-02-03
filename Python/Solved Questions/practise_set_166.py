9057da854f12817b3b1909010a0a81bb5f0dfacb
Brodevil <brodevil89@gmail.com>
2022-02-03 20:51:18 +0530

    I will continue the solution tommorow

--- Python/Solved Questions/practise_set_166.py
+++ Python/Solved Questions/practise_set_166.py
@@ -1,4 +1,4 @@
-# Competative Programming Question 165 = K-Goodness String (5pts, 7pts) (From Google KickStart 2021)
+# Competative Programming Question 165 = K-Goodness String (5pts, 7pts) (From Google KickStart 2021 Round A)
 
 """
 K-Goodness String (5pts, 7pts) |
@@ -12,6 +12,10 @@ And get the solution here, solved in python by me :}
 # Author = Abhinav
 # Date = 3 February 2022
 # Pourpose = Just for practise and imporving skills
-# Source =  [CodeForces](https://codingcompetitions.withgoogle.com/kickstart/round/0000000000436140/000000000068cca3)
+# Source =  [Google KickStart 2021 Round A](https://codingcompetitions.withgoogle.com/kickstart/round/0000000000436140/000000000068cca3)
 
 # Solution :
+
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    s = input()
