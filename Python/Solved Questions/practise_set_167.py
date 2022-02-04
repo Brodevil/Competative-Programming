c32ecfba27e7f4e8d64e6cb6c92f96cb63401e7c
Brodevil <brodevil89@gmail.com>
2022-02-04 19:29:00 +0530

    Initial Commit

---
+++ Python/Solved Questions/practise_set_167.py
@@ -0,0 +1,17 @@
+# Competative Programming Question 165 = L Shaped Plots (From Google KickStart 2021 Round A)
+
+"""
+L Shaped Plots |
+
+Get the Problem Statement on CodeForces : https://codingcompetitions.withgoogle.com/kickstart/round/0000000000436140/000000000068c509
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 3 February 2022
+# Pourpose = Just for practise and imporving skills
+# Source =  [Google KickStart 2021 Round A](Get the Problem Statement on CodeForces : https://codingcompetitions.withgoogle.com/kickstart/round/0000000000436140/000000000068c509)
+
+# Solution :
