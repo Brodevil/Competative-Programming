6c72444828ef9b9de9e39e8a9c59c255739c2d1c
Brodevil <brodevil89@gmail.com>
2022-02-07 20:55:56 +0530

    My Solution

--- Python/Solved Questions/practise_set_169.py
+++ Python/Solved Questions/practise_set_169.py
@@ -15,3 +15,18 @@ And get the solution here, solved in python by me :}
 # Source =  [CodeForces](https://codeforces.com/problemset/problem/1633/A)
 
 # Solution :
+
+def diff(i, j):
+    n = 0
+    for h, a in zip(i, j):
+        if h != a:
+            n += 1
+    return n
+
+for _ in range(int(input())):
+    n = int(input())
+    s1, s2 = str((n//7+1)*7), str((n//7)*7)
+    if diff(str(n), s1) > diff(str(n), s2):
+        print(s2)
+    else:
+        print(s1)
