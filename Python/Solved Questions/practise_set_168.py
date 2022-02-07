a64d834e9eed877b49b7e2312aedd29cc0f54388
Brodevil <brodevil89@gmail.com>
2022-02-07 20:28:56 +0530

    Solved it

--- Python/Solved Questions/practise_set_168.py
+++ Python/Solved Questions/practise_set_168.py
@@ -15,3 +15,20 @@ And get the solution here, solved in python by me :}
 # Source =  [HackerRank](https://www.hackerrank.com/challenges/larrys-array/problem)
 
 # Solution :
+
+
+for _ in range(int(input())):
+    count = 0
+    n = int(input())
+    l = list(map(int, input().split()))
+
+    for i in range(n-1):
+        for j in range(i + 1, len(l)):
+            if l[i] > l[j]:
+                count += 1
+            print(l[i], l[j], count)
+    
+    if count % 2 == 0:
+        print("YES")
+    else:
+        print("NO")
