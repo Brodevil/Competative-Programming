a1db5bf0cd4b29f11be12a43250dafe748799852
Brodevil <brodevil89@gmail.com>
2022-02-07 20:54:57 +0530

    Initial Commit

---
+++ Python/Solved Questions/practise_set_169.py
@@ -0,0 +1,17 @@
+# Competative Programming Question 169 = A. Div. 7 (From CodeForces)
+
+"""
+A. Div. 7 |
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1633/A
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 7 February 2022
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeForces](https://codeforces.com/problemset/problem/1633/A)
+
+# Solution :
