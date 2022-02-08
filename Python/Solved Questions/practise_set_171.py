801ff3c5840c8899302962f833ffac00b38f9fbe
Brodevil <brodevil89@gmail.com>
2022-02-08 20:22:44 +0530

    Initial commit

---
+++ Python/Solved Questions/practise_set_171.py
@@ -0,0 +1,17 @@
+# Competative Programming Question 171 = B. Peculiar Movie Preferences (From CodeForces)
+
+"""
+B. Peculiar Movie Preferences |
+
+Get the Problem Statement on CodeForces : https://codeforces.com/problemset/problem/1628/B
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 09 February 2022
+# Pourpose = Just for practise and imporving skills
+# Source =  [CodeForces](https://codeforces.com/problemset/problem/1628/B)
+
+# Solution :
