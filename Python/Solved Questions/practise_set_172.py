9537edc006fb52e1b3eb124585281fe1b064a0d9
Brodevil <brodevil89@gmail.com>
2022-02-09 20:08:37 +0530

    Initial commit

---
+++ Python/Solved Questions/practise_set_172.py
@@ -0,0 +1,17 @@
+# Competative Programming Question 172 = Game of Thrones - I (From HackerRank)
+
+"""
+Game of Thrones - I |
+
+Get the Problem Statement on HackerRank : https://www.hackerrank.com/challenges/game-of-thrones/problem
+
+And get the solution here, solved in python by me :}
+
+"""
+
+# Author = Abhinav
+# Date = 09 February 2022
+# Pourpose = Just for practise and imporving skills
+# Source =  [HackerRank](https://www.hackerrank.com/challenges/game-of-thrones/problem)
+
+# Solution :
