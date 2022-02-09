87b5fdc3fb82f81353e76cdb66436c393c059426
Brodevil <brodevil89@gmail.com>
2022-02-09 20:05:14 +0530

    Initial commit

--- Python/Solved Questions/practise_set_171.py
+++ Python/Solved Questions/practise_set_171.py
@@ -15,3 +15,21 @@ And get the solution here, solved in python by me :}
 # Source =  [CodeForces](https://codeforces.com/problemset/problem/1628/B)
 
 # Solution :
+
+for _it in range(int(input())):
+    n = int(input())
+    ans = "NO"
+    set1 = set()
+    set2 = set()
+
+    for i in range(n):
+        s = input()
+        ss = s[::-1]
+        if s == ss or s in set2 or s in set1 or s[1:] in set1:
+            ans = "YES"
+        set1.add(ss)
+        
+        if len(ss) == 3:
+            set2.add(ss[1:])
+    
+    print(ans)
