08f87a2bf193e7ed3404b031215ddad0c6ca2ae3
Brodevil <brodevil89@gmail.com>
2022-07-14 19:37:19 +0530

    Initial Commit

---
+++ C++/Solved Questions/problem_set_1.cpp
@@ -0,0 +1,18 @@
+// Problem Set 1 : Basic Data Types [https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem]
+
+// Author = Abhinav
+// Date = 14 July 2022
+// Source =  [HackerRank](https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem)
+
+
+// Solution :
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    
+    return 0;
+}
