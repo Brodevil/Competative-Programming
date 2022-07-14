42dc946f90bd15bbc4725bc14474dc82e22d0ee6
Brodevil <brodevil89@gmail.com>
2022-07-14 20:45:41 +0530

    Functions

--- C++/Solved Questions/problem_set_4.cpp
+++ C++/Solved Questions/problem_set_4.cpp
@@ -7,11 +7,34 @@
 // Solution :
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main() 
+int greatest(int n1, int n2, int n3, int n4)
 {
+    vector<int> nums;
+    nums.push_back(n1);
+    nums.push_back(n2);
+    nums.push_back(n3);
+    nums.push_back(n4);
     
+    int great = nums[0];
+    for (int i = 1; i < 4; i++)
+    {
+        if (great < nums[i])
+        {
+            great = nums[i];
+        }
+    }
+    return great;
+}
+
+int main()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    int great = greatest(a, b, c, d);
+    cout << great;
     return 0;
 }
