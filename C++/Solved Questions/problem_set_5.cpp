7016f09e29caff9777daf1d013651d35c143c30e
Brodevil <brodevil89@gmail.com>
2022-07-14 20:50:00 +0530

    Initial Commit

---
+++ C++/Solved Questions/problem_set_5.cpp
@@ -0,0 +1,17 @@
+// Problem Set 5 : Pointers [https://www.hackerrank.com/challenges/c-tutorial-pointer/problem]
+
+// Author = Abhinav
+// Date = 15 July 2022
+// Source =  [HackerRank](https://www.hackerrank.com/challenges/c-tutorial-pointer/problem)
+
+// Solution :
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    
+    return 0;
+}
---
+++ C++/Solved Questions/problem_set_6.cpp
@@ -0,0 +1,17 @@
+// Problem Set 6 : Arrays Introductor [https://hackerrank.com/challenges/arrays-introduction/problem]
+
+// Author = Abhinav
+// Date = 15 July 2022
+// Source =  [HackerRank](https://hackerrank.com/challenges/arrays-introduction/problem)
+
+// Solution :
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    
+    return 0;
+}
