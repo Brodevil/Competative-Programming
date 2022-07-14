2285bca510423a5c438e0294268594f5aaa1bbeb
Brodevil <brodevil89@gmail.com>
2022-07-14 20:16:48 +0530

    For Loop

--- C++/Solved Questions/problem_set_3.cpp
+++ C++/Solved Questions/problem_set_3.cpp
@@ -1,7 +1,41 @@
-// Problem Set 3 : Conditional Statements [https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else/problem]
+// Problem Set 3 : For Loop [https://hackerrank.com/challenges/c-tutorial-for-loop/problem]
 
 // Author = Abhinav
 // Date = 14 July 2022
-// Source =  [HackerRank](https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else/problem)
+// Source =  [HackerRank](https://hackerrank.com/challenges/c-tutorial-for-loop/problem)
 
 // Solution :
+
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    map<int, string> numbers;
+    numbers[0] = "Greater than 9";
+    numbers[1] = "one", numbers[2] = "two", numbers[3] = "three", numbers[4] = "four", numbers[5] = "five", numbers[6] = "six", numbers[7] = "seven", numbers[8] = "eight", numbers[9] = "nine";
+
+    int a, b;
+    cin >> a >> b;
+
+    for (a; a <= b; a++)
+    {
+        if (a <= 9)
+        {
+            cout << numbers[a] << endl;
+        }
+        else if (a % 2 == 0)
+        {
+            cout << "even" << endl;
+        }
+        else
+        {
+            cout << "odd" << endl;
+        }
+    }
+
+    return 0;
+}
