06e9596469150eae664e857f18c22499c0f9bf94
Brodevil <brodevil89@gmail.com>
2022-07-14 19:49:43 +0530

    Basic Data Types

--- C++/Solved Questions/problem_set_1.cpp
+++ C++/Solved Questions/problem_set_1.cpp
@@ -4,15 +4,26 @@
 // Date = 14 July 2022
 // Source =  [HackerRank](https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem)
 
-
 // Solution :
 
 #include <iostream>
 
 using namespace std;
 
-int main() 
+int main()
 {
-    
+    int a;
+    long int b;
+    char c;
+    float d;
+    double e;
+
+    scanf("%d %ld %c %f %lf", &a, &b, &c, &d, &e);
+    printf("%d\n", a);
+    printf("%ld\n", b);
+    printf("%c\n", c);
+    printf("%f\n", d);
+    printf("%lf", e);
+
     return 0;
 }
