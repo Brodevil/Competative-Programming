620455a180c03e550ea9d16a8b15339759da8575
Brodevil <brodevil89@gmail.com>
2022-07-16 20:33:20 +0530

    Arrys Introductor

--- C++/Solved Questions/problem_set_6.cpp
+++ C++/Solved Questions/problem_set_6.cpp
@@ -10,8 +10,16 @@
 
 using namespace std;
 
-int main() 
+int main()
 {
+    int n, i = 0;
+    cin >> n;
+    int *A = new int[n];
+    
+    while (cin >> A[i++]);
+    
+    while (cout << A[--n] << ' ' && n);
+    delete[] A;
     
     return 0;
 }
