c175f13198d25f6e5847c79f22cd8b05845d4ec6
Brodevil <brodevil89@gmail.com>
2022-07-16 20:02:57 +0530

    Pointers

--- C++/Solved Questions/problem_set_5.cpp
+++ C++/Solved Questions/problem_set_5.cpp
@@ -10,8 +10,35 @@
 
 using namespace std;
 
-int main() 
+void update(int *a, int *b)
 {
-    
+    int _a = *a;
+    int _b = *b;
+
+    if (_a > _b)
+    {
+        *b = _a - _b;
+    }
+    else if (_b > _a)
+    {
+        *b = _b - _a;
+    }
+    else
+    {
+        *b = 0;
+    }
+
+    *a = _a + _b;
+}
+
+int main()
+{
+    int a, b;
+    int *pa = &a, *pb = &b;
+
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
     return 0;
 }
