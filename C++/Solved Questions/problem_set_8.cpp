9168da9040dec5aa6b95247916b8668c86a47043
Brodevil <brodevil89@gmail.com>
2022-07-17 21:04:21 +0530

    Initial Commit

---
+++ C++/Solved Questions/problem_set_8.cpp
@@ -0,0 +1,17 @@
+// Problem Set 8 : Preprocessor Solution
+
+// Author = Abhinav
+// Date = 18 July 2022
+// Source =  [HackerRank](https://www.hackerrank.com/challenges/preprocessor-solution/problem)
+
+// Solution :
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    
+    return 0;
+}
