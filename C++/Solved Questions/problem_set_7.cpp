77afe071b108f1fd507069ff6fd59f40c263dc6e
Brodevil <brodevil89@gmail.com>
2022-07-17 20:40:39 +0530

    1st Incorrect Attempt

--- C++/Solved Questions/problem_set_7.cpp
+++ C++/Solved Questions/problem_set_7.cpp
@@ -7,11 +7,42 @@
 // Solution :
 
 #include <iostream>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
-int main() 
+int main()
 {
-    
+    int n, q;
+    cin >> n >> q;
+
+    vector<vector<int>> arr;
+    vector<int> temp;
+    string input;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> input;
+        getline(cin, input);
+        istringstream is(input);
+        int num;
+        while (is >> num)
+        {
+            temp.push_back(num);
+        }
+        temp.erase(temp.begin());
+        arr[i] = temp;
+        temp.clear();      
+        
+    }
+
+    int i, k;
+    for (int i = 0; i < q; i++)
+    {
+        cin >> i >> k;
+        cout<<arr[i][k];
+    }
+
     return 0;
 }
